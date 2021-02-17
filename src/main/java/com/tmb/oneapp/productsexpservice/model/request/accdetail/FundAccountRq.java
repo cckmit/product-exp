3c556e6ecd316451a7b50a39be7b3d4d50096a69
verdor250 <tin.@tcs.com>
2021-02-17 15:25:05 +0700

    Add validation not null

--- src/main/java/com/tmb/oneapp/productsexpservice/model/request/accdetail/FundAccountRq.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/model/request/accdetail/FundAccountRq.java
@@ -3,14 +3,20 @@ package com.tmb.oneapp.productsexpservice.model.request.accdetail;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class FundAccountRq {
+    @NotNull
     private String unitHolderNo;
+    @NotNull
     private String fundHouseCode;
+    @NotNull
     private String fundCode;
+    @NotNull
     private String tranType;
+    @NotNull
     private String serviceType;
 }
