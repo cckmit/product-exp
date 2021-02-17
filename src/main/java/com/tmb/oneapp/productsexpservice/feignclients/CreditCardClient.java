396e45a8a53144235f1abef94687c864acb8b4b5
TANUJ-TCS\Admin <tanuj.jain3@tcs.com>
2021-02-17 18:26:58 +0700

    Revert "changes credit limit"
    This reverts commit 51def2220ae04710ed6f5f6eb671633d582e265f.

--- src/main/java/com/tmb/oneapp/productsexpservice/feignclients/CreditCardClient.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/feignclients/CreditCardClient.java
@@ -1,23 +1,19 @@
 package com.tmb.oneapp.productsexpservice.feignclients;
 
-import java.util.Map;
 
+import com.tmb.oneapp.productsexpservice.model.activatecreditcard.ActivateCardResponse;
+import com.tmb.oneapp.productsexpservice.model.activatecreditcard.VerifyCvvResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-import com.tmb.common.model.TmbOneServiceResponse;
-import com.tmb.oneapp.productsexpservice.constant.ProductsExpServiceConstant;
-import com.tmb.oneapp.productsexpservice.model.activatecreditcard.ActivateCardResponse;
 import com.tmb.oneapp.productsexpservice.model.activatecreditcard.GetCardBlockCodeResponse;
 import com.tmb.oneapp.productsexpservice.model.activatecreditcard.GetCardResponse;
-import com.tmb.oneapp.productsexpservice.model.activatecreditcard.SetCreditLimitReq;
-import com.tmb.oneapp.productsexpservice.model.activatecreditcard.SetCreditLimitResp;
-import com.tmb.oneapp.productsexpservice.model.activatecreditcard.VerifyCvvResponse;
+
+import java.util.Map;
 
 @FeignClient(name = "${feign.creditcard.service.name}", url = "${feign.creditcard.service.url}")
 public interface CreditCardClient {
@@ -36,9 +32,5 @@ public interface CreditCardClient {
 	@PostMapping(value = "/apis/creditcard/credit-card/activateCreditCard/verifyCvv")
 	public ResponseEntity<VerifyCvvResponse> verifyCvv(@RequestHeader Map<String, String> headers);
 
-	@PostMapping(value = "/apis/creditcard/set-credit-limit")
-	public ResponseEntity<TmbOneServiceResponse<SetCreditLimitResp>> fetchSetCreditLimit(
-			@RequestHeader(value = ProductsExpServiceConstant.HEADER_CORRELATION_ID) String correlationID,
-			@RequestBody SetCreditLimitReq requestBodyParameter);
 
 }
