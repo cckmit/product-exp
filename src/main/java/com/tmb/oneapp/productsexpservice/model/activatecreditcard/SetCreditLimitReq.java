ee5e9997c794c7ac335d71b3c98fe7ff68942811
TANUJ-TCS\Admin <tanuj.jain3@tcs.com>
2021-02-17 18:34:00 +0700

    Revert "Revert "changes credit limit""
    This reverts commit 396e45a8a53144235f1abef94687c864acb8b4b5.

---
+++ src/main/java/com/tmb/oneapp/productsexpservice/model/activatecreditcard/SetCreditLimitReq.java
@@ -0,0 +1,28 @@
+package com.tmb.oneapp.productsexpservice.model.activatecreditcard;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@ToString
+public class SetCreditLimitReq {
+	private String mode;
+	@JsonProperty("account_id")
+	private String accountId;
+	@JsonProperty("current_credit_limit")
+	private String currentCreditLimit;
+	@JsonProperty("effective_date")
+	private String effectiveDate;
+	@JsonProperty("expiry_date")
+	private String expiryDate;
+	@JsonProperty("request_reason")
+	private String requestReason;
+	@JsonProperty("previous_credit_limit")
+	private String previousCreditLimit;
+}
