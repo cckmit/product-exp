2221e7e442dd3c1c1bdfe2962003ae1287e529f9
SUMITRAWAL-TCS\Admin <sumit.rawal-external@tcs.com>
2021-03-18 14:42:42 +0700

    Resolving sonarqube merge conflicts

--- src/main/java/com/tmb/oneapp/productsexpservice/model/cardinstallment/CardInstallmentResponse.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/model/cardinstallment/CardInstallmentResponse.java
@@ -5,15 +5,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-/**
- * @author Admin
- *
- */
-@Getter
-@Setter
+
+@Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class CardInstallmentResponse {
