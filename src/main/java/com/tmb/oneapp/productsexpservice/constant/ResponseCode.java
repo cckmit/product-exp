63d60f170dda821a0179bde45a9973c92c062791
vendor281 <napatt.kuttikun@tcs.com>
2021-03-24 11:20:27 +0700

    ONEAPP-6328 change HTTP Status, move response codes to enum, update RSL mapping

--- src/main/java/com/tmb/oneapp/productsexpservice/constant/ResponseCode.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/constant/ResponseCode.java
@@ -15,8 +15,8 @@ public enum ResponseCode implements Serializable {
 	GENERAL_ERROR("0001", "general error", Constants.SERVICE_NAME, "unknown error"),
 	FAILED("0001", "failed", Constants.SERVICE_NAME, "failed"),
 	DATA_NOT_FOUND_ERROR("0009", "DATA NOT FOUND", Constants.SERVICE_NAME, "DATA NOT FOUND"),
-	HP_RSL_SUCCESS("AST_0000", "success", Constants.SERVICE_NAME, "failed"),
-	HP_RSL_DATA_NOT_FOUND("AST_0001", "data not found", Constants.SERVICE_NAME, "failed"),
+	HP_RSL_SUCCESS("AST_0000", "success", Constants.SERVICE_NAME, "success"),
+	HP_RSL_DATA_NOT_FOUND("AST_0001", "data not found", Constants.SERVICE_NAME, "success"),
 	HP_RSL_ERROR("AST_0004", "HP and RSL error", Constants.SERVICE_NAME, "failed"),
 	HP_ERROR_CODE("AST_0003", "HP error", Constants.SERVICE_NAME, "failed"),
 	RSL_ERROR_CODE("AST_0002", "RSL error", Constants.SERVICE_NAME, "failed");
