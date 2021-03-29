4af32a3bf0296252627b72d5d7c777b680ecbcb5
Witsanu Thammayon <witsanu.t@tcs.com>
2021-03-29 17:32:33 +0700

    move configuration to properties

--- src/main/java/com/tmb/oneapp/productsexpservice/service/NotificationService.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/service/NotificationService.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -31,13 +32,16 @@ import static com.tmb.oneapp.productsexpservice.constant.ProductsExpServiceConst
 public class NotificationService {
 
 	private static final TMBLogger<NotificationService> logger = new TMBLogger<>(NotificationService.class);
-	private static final String DEFAULT_CHANNEL_TH = "ทีเอ็มบี ทัซ";
-	private static final String DEFAULT_CHANNEL_EN = "TMB Touch";
+
+	@Value("${notification-service.e-noti.default.channel.th}")
+	private String defaultChannelTh;
+	@Value("${notification-service.e-noti.default.channel.en}")
+	private String defaultChannelEn;
 
 	private NotificationServiceClient notificationClient;
 	private CustomerServiceClient customerClient;
 	private CreditCardClient creditCardClient;
-	
+
 	@Autowired
 	public NotificationService(NotificationServiceClient notificationServiceClient,
 			CustomerServiceClient customerServiceClient, CreditCardClient creditCardClient) {
@@ -50,7 +54,8 @@ public class NotificationService {
 		logger.info("xCorrelationId:{} request customer name in th and en to customer-service", xCorrelationId);
 		ResponseEntity<TmbOneServiceResponse<CustomerProfileResponseData>> response = customerClient
 				.getCustomerProfile(new HashMap<String, String>(), crmId);
-		if (HttpStatus.OK == response.getStatusCode() && Objects.nonNull(response.getBody()) && Objects.nonNull(response.getBody().getData())
+		if (HttpStatus.OK == response.getStatusCode() && Objects.nonNull(response.getBody())
+				&& Objects.nonNull(response.getBody().getData())
 				&& SUCCESS_CODE.equals(response.getBody().getStatus().getCode())) {
 			CustomerProfileResponseData customerProfileInfo = response.getBody().getData();
 
@@ -59,15 +64,15 @@ public class NotificationService {
 			if (Objects.nonNull(cardInfoResponse.getBody())
 					&& SUCCESS_CODE.equals(cardInfoResponse.getBody().getStatus().getStatusCode().toString())) {
 				GetCardResponse cardResponse = cardInfoResponse.getBody();
-				sendActivationCardEmail(customerProfileInfo.getEmailAddress(), xCorrelationId, DEFAULT_CHANNEL_EN,
-						DEFAULT_CHANNEL_TH, accountId, cardResponse.getProductCodeData().getProductNameEN(),
+				sendActivationCardEmail(customerProfileInfo.getEmailAddress(), xCorrelationId, defaultChannelEn,
+						defaultChannelTh, accountId, cardResponse.getProductCodeData().getProductNameEN(),
 						cardResponse.getProductCodeData().getProductNameTH());
 			}
 		}
 	}
 
-	private void sendActivationCardEmail(String email, String xCorrelationId, String channelNameEn, String channelNameTh,
-			String accountId, String productNameEn, String productNameTh) {
+	private void sendActivationCardEmail(String email, String xCorrelationId, String channelNameEn,
+			String channelNameTh, String accountId, String productNameEn, String productNameTh) {
 		NotificationRequest notificationRequest = new NotificationRequest();
 		List<NotificationRecord> notificationRecords = new ArrayList<>();
 		NotificationRecord emailRecord = new NotificationRecord();
