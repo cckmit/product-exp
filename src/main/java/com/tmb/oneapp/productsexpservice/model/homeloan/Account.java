aa8224dae836516851dc7253add15093c3a2877f
SUMITRAWAL-TCS\Admin <sumit.rawal-external@tcs.com>
2021-03-31 13:23:40 +0700

    Sending homeloan package to remote

---
+++ src/main/java/com/tmb/oneapp/productsexpservice/model/homeloan/Account.java
@@ -0,0 +1,51 @@
+package com.tmb.oneapp.productsexpservice.model.homeloan;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
+
+@Data
+@JsonPropertyOrder({
+        "id",
+        "type",
+        "branch_id",
+        "product_id",
+        "title",
+        "currency",
+        "status",
+        "debit_account",
+        "balances",
+        "payment",
+        "credit_limit",
+        "rates"
+})
+@JsonInclude(Include.NON_NULL)
+public class Account {
+
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("branch_id")
+    private String branchId;
+    @JsonProperty("product_id")
+    private String productId;
+    @JsonProperty("title")
+    private String title;
+    @JsonProperty("currency")
+    private String currency;
+    @JsonProperty("status")
+    private Status status;
+    @JsonProperty("debit_account")
+    private DebitAccount debitAccount;
+    @JsonProperty("balances")
+    private Balances balances;
+    @JsonProperty("payment")
+    private Payment payment;
+    @JsonProperty("credit_limit")
+    private CreditLimit creditLimit;
+    @JsonProperty("rates")
+    private Rates rates;
+}
---
+++ src/main/java/com/tmb/oneapp/productsexpservice/model/homeloan/Balances.java
@@ -0,0 +1,40 @@
+package com.tmb.oneapp.productsexpservice.model.homeloan;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
+
+@JsonInclude(Include.NON_NULL)
+@JsonPropertyOrder({
+        "original_loan",
+        "principal",
+        "ledger",
+        "outstanding",
+        "available",
+        "current",
+        "accrued_interest",
+        "payoff"
+})
+@Data
+public class Balances {
+
+    @JsonProperty("original_loan")
+    private String originalLoan;
+    @JsonProperty("principal")
+    private String principal;
+    @JsonProperty("ledger")
+    private String ledger;
+    @JsonProperty("outstanding")
+    private String outstanding;
+    @JsonProperty("available")
+    private String available;
+    @JsonProperty("current")
+    private String current;
+    @JsonProperty("accrued_interest")
+    private String accruedInterest;
+    @JsonProperty("payoff")
+    private String payoff;
+
+}
