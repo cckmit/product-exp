a08200e4550a551aa709e2eeaf9024d4f72c972c
SUMITRAWAL-TCS\Admin <sumit.rawal-external@tcs.com>
2021-04-02 09:39:49 +0700

    Adding ConversionUtilTest to improve Test cases

---
+++ src/test/java/com/tmb/oneapp/productsexpservice/util/ConversionUtilTest.java
@@ -0,0 +1,31 @@
+package com.tmb.oneapp.productsexpservice.util;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.math.BigDecimal;
+@RunWith(JUnit4.class)
+public class ConversionUtilTest {
+
+    @Test
+    public void testStringToDouble() throws Exception {
+        Double result = ConversionUtil.stringToDouble("10.0");
+        Assertions.assertEquals(10.0, result);
+    }
+
+    @Test
+    public void testBigDecimalToDouble() throws Exception {
+        Double result = ConversionUtil.bigDecimalToDouble(new BigDecimal(1000000.00));
+        Assertions.assertEquals(1000000.00, result);
+    }
+
+    @Test
+    public void testDoubleToString() throws Exception {
+        String result = ConversionUtil.doubleToString(Double.valueOf(100));
+        Assert.assertEquals("100.0",result);
+    }
+}
+
