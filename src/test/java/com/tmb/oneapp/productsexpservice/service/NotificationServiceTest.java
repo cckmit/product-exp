0b661eb4510841f65539466ac22f6d6e340775a8
SUMITRAWAL-TCS\Admin <sumit.rawal-external@tcs.com>
2021-04-05 17:07:21 +0700

    adding line

--- src/test/java/com/tmb/oneapp/productsexpservice/service/NotificationServiceTest.java
+++ src/test/java/com/tmb/oneapp/productsexpservice/service/NotificationServiceTest.java
@@ -342,6 +342,7 @@ public class NotificationServiceTest {
 		String correlationId="1234";
 		ResponseEntity<FetchCardResponse> cardInfoResponse = creditCardClient.getCreditCardDetails(correlationId,
 				accountId);
+		
 		when(notificationServiceClient.sendMessage(anyString(), any())).thenReturn(response);
 
 		when(customerServiceClient.getCustomerProfile(any(), any()))
