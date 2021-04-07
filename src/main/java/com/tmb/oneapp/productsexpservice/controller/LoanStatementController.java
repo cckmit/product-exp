2b29d0f87ccff3c1a937a6d2daefcab02db9adda
SUMITRAWAL-TCS\Admin <sumit.rawal-external@tcs.com>
2021-04-07 18:30:08 +0700

    Resolving code smells in sonarqube

--- src/main/java/com/tmb/oneapp/productsexpservice/controller/LoanStatementController.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/controller/LoanStatementController.java
@@ -22,9 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.time.Instant;
-import java.util.Map;
 
 @RestController
 @Api(tags = "Fetch Home loan account statement")
