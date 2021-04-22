24d57a94bdcba5cf47831fe799ef611b68221419
vendor286 <sorawit.s@tcs.com>
2021-04-22 18:44:33 +0700

    update

--- src/test/java/com/tmb/oneapp/productsexpservice/controller/NcbPaymentConfirmControllerTest.java
+++ src/test/java/com/tmb/oneapp/productsexpservice/controller/NcbPaymentConfirmControllerTest.java
@@ -1,7 +1,6 @@
 package com.tmb.oneapp.productsexpservice.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tmb.common.exception.model.TMBCommonException;
 import com.tmb.common.model.TmbOneServiceResponse;
