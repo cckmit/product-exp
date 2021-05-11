5a4ae1953e16f53afdd4f06c3ba60dc7a92410b4
vendor249 <1919043@tcs.com>
2021-05-11 13:13:52 +0700

    add unit test

--- src/test/java/com/tmb/oneapp/productsexpservice/service/ProductsExpServiceTest.java
+++ src/test/java/com/tmb/oneapp/productsexpservice/service/ProductsExpServiceTest.java
@@ -115,7 +115,73 @@ public class ProductsExpServiceTest {
         Assert.assertEquals(expectedResponse.getBody().getFundClassList()
                 .getFundClass().size(), result.getFundClass().size());
         Assert.assertEquals(Boolean.TRUE,result.getIsPtes());
+        Assert.assertEquals(0,result.getSmartPortList().size());
+        Assert.assertEquals(expectedResponse.getBody().getFundClassList()
+                .getFundClass().size(), result.getPtPortList().size());
     }
+
+    @Test
+    public void testGetFundSummaryWithSmartPort() throws Exception {
+
+        FundSummaryRq rq = new FundSummaryRq();
+        rq.setCrmId("test");
+        String corrID = "32fbd3b2-3f97-4a89-ae39-b4f628fbc8da";
+        FundSummaryResponse expectedResponse = new FundSummaryResponse();
+        FundSummaryByPortResponse fundSummaryByPortResponse;
+        TmbOneServiceResponse<FundSummaryResponse> oneServiceResponse = new TmbOneServiceResponse<>();
+        TmbOneServiceResponse<List<PtesDetail>> oneServiceResponsePtes = new TmbOneServiceResponse<>();
+        List<PtesDetail> ptesDetailList = null ;
+        TmbOneServiceResponse<FundSummaryByPortResponse> portResponse = new TmbOneServiceResponse<>();
+
+        try {
+            FileInputStream fis = new FileInputStream("src/test/resources/investment/investment_port_list.txt");
+            String data = IOUtils.toString(fis, "UTF-8");
+            ObjectMapper mapper = new ObjectMapper();
+            expectedResponse = mapper.readValue(Paths.get("src/test/resources/investment/fund_summary_data_with_smart_port.json").toFile(),
+                    FundSummaryResponse.class);
+            ObjectMapper mapperPort = new ObjectMapper();
+            fundSummaryByPortResponse = mapperPort.readValue(Paths.get("src/test/resources/investment/fund_summary_by_port.json").toFile(),
+                    FundSummaryByPortResponse.class);
+            ptesDetailList =  mapperPort.readValue(Paths.get("src/test/resources/investment/ptest.json").toFile(),
+                    new TypeReference <List<PtesDetail>>() {
+                    });
+            oneServiceResponse.setData(expectedResponse);
+            oneServiceResponse.setStatus(new TmbStatus(ProductsExpServiceConstant.SUCCESS_CODE,
+                    ProductsExpServiceConstant.SUCCESS_MESSAGE,
+                    ProductsExpServiceConstant.SERVICE_NAME, ProductsExpServiceConstant.SUCCESS_MESSAGE));
+
+            oneServiceResponsePtes.setData(ptesDetailList);
+            oneServiceResponsePtes.setStatus(new TmbStatus(ProductsExpServiceConstant.SUCCESS_CODE,
+                    ProductsExpServiceConstant.SUCCESS_MESSAGE,
+                    ProductsExpServiceConstant.SERVICE_NAME, ProductsExpServiceConstant.SUCCESS_MESSAGE));
+
+            portResponse.setData(fundSummaryByPortResponse);
+            portResponse.setStatus(new TmbStatus(ProductsExpServiceConstant.SUCCESS_CODE,
+                    ProductsExpServiceConstant.SUCCESS_MESSAGE,
+                    ProductsExpServiceConstant.SERVICE_NAME, ProductsExpServiceConstant.SUCCESS_MESSAGE));
+
+
+            when(investmentRequestClient.callInvestmentFundSummaryService(any(), any()))
+                    .thenReturn(ResponseEntity.ok().headers(TMBUtils.getResponseHeaders()).body(oneServiceResponse));
+            when(investmentRequestClient.callInvestmentFundSummaryByPortService(any(), any()))
+                    .thenReturn(ResponseEntity.ok().headers(TMBUtils.getResponseHeaders()).body(portResponse));
+
+            when(customerExpServiceClient.getAccountSaving(any(), anyString())).thenReturn(data);
+            when(investmentRequestClient.getPtesPort(any(),any())).thenReturn(ResponseEntity.ok().headers(TMBUtils.getResponseHeaders())
+                    .body(oneServiceResponsePtes));
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        FundSummaryBody result = productsExpService.getFundSummary(corrID, rq);
+        Assert.assertEquals(expectedResponse.getBody().getFundClassList()
+                .getFundClass().size(), result.getFundClass().size());
+        Assert.assertEquals(Boolean.TRUE,result.getIsPtes());
+        Assert.assertEquals(2,result.getSmartPortList().size());
+
+    }
+
 
 
     @Test
