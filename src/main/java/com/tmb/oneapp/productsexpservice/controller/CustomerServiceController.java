5b3366d9bbdac5fa1282956701e9ac4f8fe86e4d
Witsanu Thammayon <witsanu.t@tcs.com>
2021-05-12 15:38:01 +0700

    add service for expose

--- src/main/java/com/tmb/oneapp/productsexpservice/controller/CustomerServiceController.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/controller/CustomerServiceController.java
@@ -1,21 +1,28 @@
 package com.tmb.oneapp.productsexpservice.controller;
 
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tmb.common.logger.LogAround;
 import com.tmb.common.logger.TMBLogger;
 import com.tmb.common.model.TmbOneServiceResponse;
 import com.tmb.common.model.TmbStatus;
+import com.tmb.common.model.address.Province;
 import com.tmb.oneapp.productsexpservice.constant.ProductsExpServiceConstant;
 import com.tmb.oneapp.productsexpservice.constant.ResponseCode;
+import com.tmb.oneapp.productsexpservice.feignclients.CommonServiceClient;
 import com.tmb.oneapp.productsexpservice.model.flexiloan.CustIndividualProfileInfo;
+import com.tmb.oneapp.productsexpservice.model.request.AddressCommonSearchReq;
 import com.tmb.oneapp.productsexpservice.service.CustomerProfileService;
 
 import io.swagger.annotations.Api;
@@ -28,10 +35,13 @@ public class CustomerServiceController {
 	private static final TMBLogger<CustomerServiceController> logger = new TMBLogger<>(CustomerServiceController.class);
 
 	private CustomerProfileService customerProfileService;
+	private CommonServiceClient commonServiceClient;
 
 	@Autowired
-	public CustomerServiceController(CustomerProfileService customerProfileService) {
+	public CustomerServiceController(CustomerProfileService customerProfileService,
+			CommonServiceClient commonServiceClient) {
 		this.customerProfileService = customerProfileService;
+		this.commonServiceClient = commonServiceClient;
 	}
 
 	@LogAround
@@ -43,11 +53,23 @@ public class CustomerServiceController {
 		TmbOneServiceResponse<CustIndividualProfileInfo> customerIndividualProfileInfo = new TmbOneServiceResponse<>();
 		CustIndividualProfileInfo individualProfileInfo = customerProfileService.getIndividualProfile(crmId);
 		customerIndividualProfileInfo.setData(individualProfileInfo);
-		customerIndividualProfileInfo.setStatus(new TmbStatus(ResponseCode.SUCESS.getCode(), ResponseCode.SUCESS.getMessage(),
-                    ResponseCode.SUCESS.getService(), ResponseCode.SUCESS.getDesc()));
-				return ResponseEntity.ok().body(customerIndividualProfileInfo);
+		customerIndividualProfileInfo.setStatus(new TmbStatus(ResponseCode.SUCESS.getCode(),
+				ResponseCode.SUCESS.getMessage(), ResponseCode.SUCESS.getService(), ResponseCode.SUCESS.getDesc()));
+		return ResponseEntity.ok().body(customerIndividualProfileInfo);
 	}
-	
-	
+
+	@LogAround
+	@GetMapping(value = "/zipcode")
+	@ApiOperation(value = "Get Address info details by post code")
+	public ResponseEntity<TmbOneServiceResponse<List<Province>>> getMasterAddessInfo(
+			@RequestParam(value = "code") String postCode) {
+		if (StringUtils.isEmpty(postCode)) {
+			return ResponseEntity.ok().build();
+		}
+		AddressCommonSearchReq searchReq = new AddressCommonSearchReq();
+		searchReq.setField("postcode");
+		searchReq.setSearch(postCode);
+		return commonServiceClient.searchAddressByField(searchReq);
+	}
 
 }
