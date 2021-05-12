0d887d87221e74ca9bc46ad3d4bcb119ce6e5914
Witsanu Thammayon <witsanu.t@tcs.com>
2021-05-12 14:49:58 +0700

    commit product exp change

--- src/main/java/com/tmb/oneapp/productsexpservice/controller/CustomerServiceController.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/controller/CustomerServiceController.java
@@ -1,10 +1,8 @@
 package com.tmb.oneapp.productsexpservice.controller;
 
-import java.time.Instant;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +47,7 @@ public class CustomerServiceController {
                     ResponseCode.SUCESS.getService(), ResponseCode.SUCESS.getDesc()));
 				return ResponseEntity.ok().body(customerIndividualProfileInfo);
 	}
+	
+	
 
 }
