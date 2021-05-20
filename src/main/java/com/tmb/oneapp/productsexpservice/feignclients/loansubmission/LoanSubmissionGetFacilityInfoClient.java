b6ea4544ba53ac12fe558584c8436cba5368390b
Oranuch Charoennan <oranuch@odds.team>
2021-05-20 14:52:13 +0700

    add loan summission client

---
+++ src/main/java/com/tmb/oneapp/productsexpservice/feignclients/loansubmission/LoanSubmissionGetFacilityInfoClient.java
@@ -0,0 +1,47 @@
+package com.tmb.oneapp.productsexpservice.feignclients.loansubmission;
+
+import com.tmb.common.model.legacy.rsl.ws.facility.request.Body;
+import com.tmb.common.model.legacy.rsl.ws.facility.request.Header;
+import com.tmb.common.model.legacy.rsl.ws.facility.request.RequestFacility;
+import com.tmb.common.model.legacy.rsl.ws.facility.response.ResponseFacility;
+import com.tmb.common.model.legacy.rsl.ws.loan.submission.*;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.xml.rpc.ServiceException;
+import java.rmi.RemoteException;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class LoanSubmissionGetFacilityInfoClient {
+    @Value("${loan-submission-get-facility-info.url}")
+    private String getFacilityInfoUrl;
+
+    LoanSubmissionGetFacilityInfoServiceLocator locator = new LoanSubmissionGetFacilityInfoServiceLocator();
+
+    private static final String CHANNEL = "MIB";
+    private static final String MODULE = "3";
+
+    public ResponseFacility searchFacilityInfoByCaID(Long caID) throws RemoteException, ServiceException {
+        locator.setLoanSubmissionGetFacilityInfoEndpointAddress(getFacilityInfoUrl);
+
+        LoanSubmissionGetFacilityInfoSoapBindingStub stub = (LoanSubmissionGetFacilityInfoSoapBindingStub) locator.getLoanSubmissionGetFacilityInfo();
+
+        RequestFacility req = new RequestFacility();
+
+        Header header = new Header();
+        header.setChannel(CHANNEL);
+        header.setModule(MODULE);
+        header.setRequestID(UUID.randomUUID().toString());
+        req.setHeader(header);
+
+        Body body = new Body();
+        body.setCaID(caID);
+        req.setBody(body);
+
+        return stub.searchFacilityInfoByCaID(req);
+    }
+
+}
