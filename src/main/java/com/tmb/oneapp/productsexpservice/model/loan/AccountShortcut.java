f9811412e183c15e644bfdd8ccad642f220fbd76
Oranuch Charoennan <oranuch@odds.team>
2021-05-25 19:44:30 +0700

    clean code

--- src/main/java/com/tmb/oneapp/productsexpservice/model/loan/AccountShortcut.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/model/loan/AccountShortcut.java
@@ -1,6 +1,5 @@
 package com.tmb.oneapp.productsexpservice.model.loan;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
