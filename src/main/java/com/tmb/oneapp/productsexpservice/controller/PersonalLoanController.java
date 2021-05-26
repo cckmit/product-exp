8e0c657d3724d19220f1860d482f27e7d79f4793
kamonwan <kamonwan294@gmail.com>
2021-05-26 15:34:47 +0700

    add unit test personal loan controller

--- src/main/java/com/tmb/oneapp/productsexpservice/controller/PersonalLoanController.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/controller/PersonalLoanController.java
@@ -4,10 +4,13 @@ import com.tmb.common.logger.LogAround;
 import com.tmb.common.logger.TMBLogger;
 import com.tmb.common.model.TmbOneServiceResponse;
 import com.tmb.common.model.TmbStatus;
+import com.tmb.common.model.legacy.rsl.ws.instant.calculate.uw.request.RequestInstantLoanCalUW;
+import com.tmb.common.model.legacy.rsl.ws.instant.calculate.uw.response.ResponseInstantLoanCalUW;
 import com.tmb.oneapp.productsexpservice.constant.ProductsExpServiceConstant;
 import com.tmb.oneapp.productsexpservice.constant.ResponseCode;
 import com.tmb.oneapp.productsexpservice.model.request.loan.LoanPreloadRequest;
 import com.tmb.oneapp.productsexpservice.model.response.LoanPreloadResponse;
+import com.tmb.oneapp.productsexpservice.service.LoanSubmissionInstantLoanCalUWService;
 import com.tmb.oneapp.productsexpservice.service.PersonalLoanService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -15,9 +18,12 @@ import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
+import javax.xml.rpc.ServiceException;
+import java.rmi.RemoteException;
 import java.time.Instant;
 
 @RestController
@@ -26,8 +32,8 @@ import java.time.Instant;
 @Api(tags = "Personal Loan")
 public class PersonalLoanController {
 
-
     private final PersonalLoanService personalLoanService;
+    private final LoanSubmissionInstantLoanCalUWService loanCalUWService;
     private static final TMBLogger<ProductsVerifyCvvController> logger = new TMBLogger<>(ProductsVerifyCvvController.class);
 
     @GetMapping("/get_preload")
@@ -36,7 +42,7 @@ public class PersonalLoanController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "X-Correlation-ID", defaultValue = "32fbd3b2-3f97-4a89-ae39-b4f628fbc8da", required = true, dataType = "string", paramType = "header", example = "32fbd3b2-3f97-4a89-ae39-b4f628fbc8da")
     })
-    public ResponseEntity<TmbOneServiceResponse<LoanPreloadResponse>> checkPreload( @Valid @RequestHeader(ProductsExpServiceConstant.HEADER_CORRELATION_ID) String correlationId,
+    public ResponseEntity<TmbOneServiceResponse<LoanPreloadResponse>> checkPreload(@Valid @RequestHeader(ProductsExpServiceConstant.HEADER_CORRELATION_ID) String correlationId,
                                                                                         @Valid LoanPreloadRequest loanPreloadRequest) {
         TmbOneServiceResponse<LoanPreloadResponse> oneTmbOneServiceResponse = new TmbOneServiceResponse<>();
         HttpHeaders responseHeaders = new HttpHeaders();
@@ -58,6 +64,30 @@ public class PersonalLoanController {
         }
 
 
+    }
+
+    @GetMapping(value = "/get-preload-brms", produces = MediaType.APPLICATION_JSON_VALUE)
+    @LogAround
+    @ApiOperation("Get preload brms")
+    public ResponseEntity<TmbOneServiceResponse<ResponseInstantLoanCalUW>> checkCalUW(@Valid RequestInstantLoanCalUW request) throws ServiceException, RemoteException {
+        TmbOneServiceResponse<ResponseInstantLoanCalUW> oneTmbOneServiceResponse = new TmbOneServiceResponse<>();
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set(ProductsExpServiceConstant.HEADER_TIMESTAMP, String.valueOf(Instant.now().toEpochMilli()));
+
+        try {
+            ResponseInstantLoanCalUW responseInstantLoanCalUW = loanCalUWService.checkCalculateUnderwriting(request);
+            oneTmbOneServiceResponse.setData(responseInstantLoanCalUW);
+            oneTmbOneServiceResponse.setStatus(new TmbStatus(ProductsExpServiceConstant.SUCCESS_CODE,
+                    ProductsExpServiceConstant.SUCCESS_MESSAGE,
+                    ProductsExpServiceConstant.SERVICE_NAME, ProductsExpServiceConstant.SUCCESS_MESSAGE));
+            responseHeaders.set("Timestamp", String.valueOf(Instant.now().toEpochMilli()));
+            return ResponseEntity.ok().body(oneTmbOneServiceResponse);
+        }catch (Exception e) {
+            logger.error("Error while getConfig: {}", e);
+            oneTmbOneServiceResponse.setStatus(new TmbStatus(ResponseCode.FAILED.getCode(), ResponseCode.FAILED.getMessage(),
+                    ResponseCode.FAILED.getService()));
+            return ResponseEntity.badRequest().headers(responseHeaders).body(oneTmbOneServiceResponse);
+        }
     }
 
 }
--- src/test/java/com/tmb/oneapp/productsexpservice/controller/PersonalLoanControllerTest.java
+++ src/test/java/com/tmb/oneapp/productsexpservice/controller/PersonalLoanControllerTest.java
@@ -1,22 +1,32 @@
 package com.tmb.oneapp.productsexpservice.controller;
 
-import static org.junit.Assert.assertTrue;
-
+import com.tmb.common.model.TmbOneServiceResponse;
+import com.tmb.common.model.legacy.rsl.ws.instant.calculate.uw.request.Body;
+import com.tmb.common.model.legacy.rsl.ws.instant.calculate.uw.request.Header;
+import com.tmb.common.model.legacy.rsl.ws.instant.calculate.uw.request.RequestInstantLoanCalUW;
+import com.tmb.common.model.legacy.rsl.ws.instant.calculate.uw.response.ResponseInstantLoanCalUW;
+import com.tmb.oneapp.productsexpservice.constant.ResponseCode;
+import com.tmb.oneapp.productsexpservice.model.request.loan.LoanPreloadRequest;
+import com.tmb.oneapp.productsexpservice.model.response.LoanPreloadResponse;
+import com.tmb.oneapp.productsexpservice.service.LoanSubmissionInstantLoanCalUWService;
+import com.tmb.oneapp.productsexpservice.service.PersonalLoanService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import com.tmb.oneapp.productsexpservice.model.request.loan.LoanPreloadRequest;
-import com.tmb.oneapp.productsexpservice.model.response.LoanPreloadResponse;
-import com.tmb.oneapp.productsexpservice.service.PersonalLoanService;
+import javax.xml.rpc.ServiceException;
+import java.math.BigDecimal;
+import java.rmi.RemoteException;
+import java.util.UUID;
 
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.when;
 
 @RunWith(JUnit4.class)
@@ -26,21 +36,76 @@ public class PersonalLoanControllerTest {
 	
 	@Mock
 	PersonalLoanService personalLoanService;
+	@Mock
+	LoanSubmissionInstantLoanCalUWService loanCalUWService;
 
 	@BeforeEach
 	void setUp() {
 		MockitoAnnotations.initMocks(this);
-		personalLoanController = new PersonalLoanController(personalLoanService);
+		personalLoanController = new PersonalLoanController(personalLoanService,loanCalUWService);
 	}
 	
 	@Test
-	public void testController() {
-		LoanPreloadRequest loadnPreloadReq = new LoanPreloadRequest();
-		loadnPreloadReq.setProductCode("P");
+	public void testCheckPreloadSuccess() {
+		LoanPreloadRequest loadPreloadReq = new LoanPreloadRequest();
+		loadPreloadReq.setProductCode("P");
 		LoanPreloadResponse response = new LoanPreloadResponse();
 		when(personalLoanService.checkPreload(any(), any())).thenReturn(response);
-		personalLoanController.checkPreload("zxx", loadnPreloadReq);
+		personalLoanController.checkPreload("zxx", loadPreloadReq);
 		assertTrue(true);
+	}
+
+	@Test
+	public void testCheckPreloadFail() {
+		LoanPreloadRequest loadPreloadReq = new LoanPreloadRequest();
+		loadPreloadReq.setProductCode("P");
+		LoanPreloadResponse response = new LoanPreloadResponse();
+		when(personalLoanService.checkPreload(any(), any())).thenThrow(new IllegalArgumentException());
+		ResponseEntity<TmbOneServiceResponse<LoanPreloadResponse>> result= personalLoanController.checkPreload("zxx", loadPreloadReq);
+		assertTrue(result.getStatusCode().isError());
+	}
+
+	@Test
+	public void testCheckCalUWSuccess() throws ServiceException, RemoteException {
+		RequestInstantLoanCalUW request = new RequestInstantLoanCalUW();
+		Body body = new Body();
+		body.setTriggerFlag("Y");
+		body.setCaId(BigDecimal.TEN);
+
+		Header header = new Header();
+		header.setChannel("MIB");
+		header.setModule("3");
+		header.setRequestID(UUID.randomUUID().toString());
+
+		request.setBody(body);
+		request.setHeader(header);
+
+		when(loanCalUWService.checkCalculateUnderwriting(request)).thenReturn(any());
+
+		ResponseEntity<TmbOneServiceResponse<ResponseInstantLoanCalUW>> result = personalLoanController.checkCalUW(request);
+		assertEquals(HttpStatus.OK.value(), result.getStatusCode().value());
+
+	}
+
+	@Test
+	public void testCheckCalUWSFail() throws ServiceException, RemoteException {
+		RequestInstantLoanCalUW request = new RequestInstantLoanCalUW();
+		Body body = new Body();
+		body.setTriggerFlag("Y");
+		body.setCaId(BigDecimal.TEN);
+
+		Header header = new Header();
+		header.setChannel("MIB");
+		header.setModule("3");
+		header.setRequestID(UUID.randomUUID().toString());
+
+		request.setBody(body);
+		request.setHeader(header);
+
+		when(loanCalUWService.checkCalculateUnderwriting(request)).thenThrow(new NullPointerException());
+
+		ResponseEntity<TmbOneServiceResponse<ResponseInstantLoanCalUW>> result = personalLoanController.checkCalUW(request);
+		assertTrue(result.getStatusCode().isError());
 	}
 
 }
