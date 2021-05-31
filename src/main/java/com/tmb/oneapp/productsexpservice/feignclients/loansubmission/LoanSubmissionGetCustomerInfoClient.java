99f3b7e053cd0db08a87f41f42736d04b21d99b0
Oranuch Charoennan <oranuch@odds.team>
2021-05-31 16:54:51 +0700

    fix merge

--- src/main/java/com/tmb/oneapp/productsexpservice/feignclients/loansubmission/LoanSubmissionGetCustomerInfoClient.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/feignclients/loansubmission/LoanSubmissionGetCustomerInfoClient.java
@@ -1,9 +1,9 @@
 package com.tmb.oneapp.productsexpservice.feignclients.loansubmission;
 
-import com.tmb.common.model.legacy.rsl.ws.instant.eligible.customer.request.Body;
-import com.tmb.common.model.legacy.rsl.ws.instant.eligible.customer.request.Header;
-import com.tmb.common.model.legacy.rsl.ws.instant.eligible.customer.request.RequestInstantLoanGetCustInfo;
-import com.tmb.common.model.legacy.rsl.ws.instant.eligible.customer.response.ResponseInstantLoanGetCustInfo;
+import com.tmb.common.model.legacy.rsl.ws.individual.request.Body;
+import com.tmb.common.model.legacy.rsl.ws.individual.request.Header;
+import com.tmb.common.model.legacy.rsl.ws.individual.request.RequestIndividual;
+import com.tmb.common.model.legacy.rsl.ws.individual.response.ResponseIndividual;
 import com.tmb.common.model.legacy.rsl.ws.loan.submission.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -17,22 +17,22 @@ public class LoanSubmissionGetCustomerInfoClient {
     @Value("${loan-submission-get-customer-info.url}")
     private String getCustomerInfoUrl;
 
-    LoanSubmissionInstantLoanGetCustomerInfoServiceLocator locator = new LoanSubmissionInstantLoanGetCustomerInfoServiceLocator();
+    LoanSubmissionGetCustomerInfoServiceLocator locator = new LoanSubmissionGetCustomerInfoServiceLocator();
 
     private static final String CHANNEL = "MIB";
     private static final String MODULE = "3";
 
-    public void setLocator(LoanSubmissionInstantLoanGetCustomerInfoServiceLocator locator) {
+    public void setLocator(LoanSubmissionGetCustomerInfoServiceLocator locator) {
         this.locator = locator;
     }
 
-    public ResponseInstantLoanGetCustInfo searchCustomerInfoByCaID(String caID) throws RemoteException, ServiceException {
-        locator.setLoanSubmissionInstantLoanGetCustomerInfoEndpointAddress(getCustomerInfoUrl);
+    public ResponseIndividual searchCustomerInfoByCaID(long caID) throws ServiceException, RemoteException, ServiceException {
+        locator.setLoanSubmissionGetCustomerInfoEndpointAddress(getCustomerInfoUrl);
 
-        LoanSubmissionInstantLoanGetCustomerInfoSoapBindingStub stub = (LoanSubmissionInstantLoanGetCustomerInfoSoapBindingStub) locator
-                .getLoanSubmissionInstantLoanGetCustomerInfo();
+        LoanSubmissionGetCustomerInfoSoapBindingStub stub = (LoanSubmissionGetCustomerInfoSoapBindingStub) locator.
+                getLoanSubmissionGetCustomerInfo();
 
-        RequestInstantLoanGetCustInfo req = new RequestInstantLoanGetCustInfo();
+        RequestIndividual req = new RequestIndividual();
 
         Header header = new Header();
         header.setChannel(CHANNEL);
@@ -41,10 +41,10 @@ public class LoanSubmissionGetCustomerInfoClient {
         req.setHeader(header);
 
         Body body = new Body();
-        body.setRmNo(caID);
+        body.setCaID(caID);
         req.setBody(body);
 
-        return stub.getInstantCustomerInfo(req);
+        return stub.searchCustomerInfoByCaID(req);
     }
 
 }
