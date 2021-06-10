2ae9e99b0036f9ee26b9d55d12002b4cae4d17a1
Nok <omerta@odds.team>
2021-06-10 17:57:22 +0700

    api get incomeInfo and unit test

---
+++ src/main/java/com/tmb/oneapp/productsexpservice/controller/LoanSubmissionOnlineController.java
@@ -0,0 +1,56 @@
+package com.tmb.oneapp.productsexpservice.controller;
+
+
+import com.tmb.common.logger.LogAround;
+import com.tmb.common.logger.TMBLogger;
+import com.tmb.common.model.TmbOneServiceResponse;
+import com.tmb.common.model.TmbStatus;
+import com.tmb.common.model.legacy.rsl.ws.incomemodel.response.ResponseIncomeModel;
+import com.tmb.oneapp.productsexpservice.constant.ProductsExpServiceConstant;
+import com.tmb.oneapp.productsexpservice.constant.ResponseCode;
+import com.tmb.oneapp.productsexpservice.model.request.loan.LoanPreloadRequest;
+import com.tmb.oneapp.productsexpservice.model.response.IncomeInfo;
+import com.tmb.oneapp.productsexpservice.model.response.LoanPreloadResponse;
+import com.tmb.oneapp.productsexpservice.service.LoanSubmissionIncomeInfoService;
+import io.swagger.annotations.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.Instant;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/online")
+@Api(tags = "waive income")
+public class LoanSubmissionOnlineController {
+    private final LoanSubmissionIncomeInfoService loanSubmissionIncomeInfoService;
+    private static final TMBLogger<LoanSubmissionOnlineController> logger = new TMBLogger<>(LoanSubmissionOnlineController.class);
+
+
+    @GetMapping("/get_income_info")
+    @LogAround
+    @ApiOperation(value = "get income info")
+    public ResponseEntity<TmbOneServiceResponse<IncomeInfo>> getIncomeInfo(@ApiParam(value = "CRMID", defaultValue = "001100000000000000000001184383", required = true) @RequestHeader(name = "X-CRMID", required = false) String crmId) {
+        TmbOneServiceResponse<IncomeInfo> oneTmbOneServiceResponse = new TmbOneServiceResponse<>();
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set(ProductsExpServiceConstant.HEADER_TIMESTAMP, String.valueOf(Instant.now().toEpochMilli()));
+        try {
+
+            oneTmbOneServiceResponse.setData(loanSubmissionIncomeInfoService.getIncomeInfoByRmId(crmId));
+            oneTmbOneServiceResponse.setStatus(new TmbStatus(ProductsExpServiceConstant.SUCCESS_CODE,
+                    ProductsExpServiceConstant.SUCCESS_MESSAGE,
+                    ProductsExpServiceConstant.SERVICE_NAME, ProductsExpServiceConstant.SUCCESS_MESSAGE));
+
+            responseHeaders.set("Timestamp", String.valueOf(Instant.now().toEpochMilli()));
+            return ResponseEntity.ok().body(oneTmbOneServiceResponse);
+        } catch (Exception e) {
+            logger.error("Error while getConfig: {}", e);
+            oneTmbOneServiceResponse.setStatus(new TmbStatus(ResponseCode.FAILED.getCode(), ResponseCode.FAILED.getMessage(),
+                    ResponseCode.FAILED.getService()));
+            return ResponseEntity.badRequest().headers(responseHeaders).body(oneTmbOneServiceResponse);
+        }
+    }
+}
---
+++ src/main/java/com/tmb/oneapp/productsexpservice/service/LoanSubmissionIncomeInfoService.java
@@ -0,0 +1,33 @@
+package com.tmb.oneapp.productsexpservice.service;
+
+import com.tmb.common.logger.TMBLogger;
+import com.tmb.common.model.legacy.rsl.ws.incomemodel.response.ResponseIncomeModel;
+import com.tmb.oneapp.productsexpservice.feignclients.loansubmission.LoanSubmissionGetIncomeModelInfoClient;
+import com.tmb.oneapp.productsexpservice.model.response.IncomeInfo;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.xml.rpc.ServiceException;
+import java.math.BigDecimal;
+import java.rmi.RemoteException;
+
+@Service
+@AllArgsConstructor
+public class LoanSubmissionIncomeInfoService {
+    private static final TMBLogger<LoanSubmissionIncomeInfoService> logger = new TMBLogger<>(LoanSubmissionIncomeInfoService.class);
+    private final LoanSubmissionGetIncomeModelInfoClient incomeModelInfoClient;
+
+
+    public IncomeInfo getIncomeInfoByRmId(String rmId)throws ServiceException, RemoteException {
+        try {
+
+            ResponseIncomeModel responseIncomeModel = incomeModelInfoClient.getIncomeInfo(rmId);
+            IncomeInfo incomeInfo = new IncomeInfo();
+            incomeInfo.setIncomeAmount(responseIncomeModel.getBody().getIncomeModelAmt());
+            return incomeInfo;
+        } catch (Exception e) {
+            logger.error("calculateUnderwriting got exception:{}", e);
+            throw e;
+        }
+    }
+}
