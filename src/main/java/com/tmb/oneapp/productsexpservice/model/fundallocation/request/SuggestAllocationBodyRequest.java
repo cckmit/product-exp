c8a1dcb318f92935a8a2c462d82f9e59612a8ab2
vendor376 <mattapong.t@tcs.com>
2021-06-11 18:58:03 +0700

    add investment client of suggestallocation service

--- src/main/java/com/tmb/oneapp/productsexpservice/model/fundallocation/request/SuggestAllocationBodyRequest.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/model/fundallocation/request/SuggestAllocationBodyRequest.java
@@ -2,9 +2,9 @@ package com.tmb.oneapp.productsexpservice.model.fundallocation.request;
 
 import lombok.Data;
 
-@Data
+import javax.validation.constraints.NotNull;
 
+@Data
 public class SuggestAllocationBodyRequest {
-    private String crmId;
-    private String suitabilityScore;
+    private @NotNull String crmId;
 }
--- src/main/java/com/tmb/oneapp/productsexpservice/model/fundallocation/response/SuggestAllocationBodyResponse.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/model/fundallocation/response/SuggestAllocationBodyResponse.java
@@ -2,6 +2,7 @@ package com.tmb.oneapp.productsexpservice.model.fundallocation.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.tmb.oneapp.productsexpservice.model.fundsummarydata.response.fundsummary.FundClassList;
+import com.tmb.oneapp.productsexpservice.model.response.fund.fundallocation.FundAllocationResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,4 +18,5 @@ import javax.validation.constraints.NotNull;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SuggestAllocationBodyResponse {
     private FundClassList fundClassList;
+    private FundAllocationResponse fundAllocation;
 }
