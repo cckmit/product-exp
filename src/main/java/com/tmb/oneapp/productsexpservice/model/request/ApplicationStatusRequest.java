312c358f4e8b15c7dc1ced4c94ac2072eb64f110
vendor281 <napatt.kuttikun@tcs.com>
2021-06-14 16:41:06 +0700

    ONEAPP-12325

---
+++ src/main/java/com/tmb/oneapp/productsexpservice/model/request/ApplicationStatusRequest.java
@@ -0,0 +1,15 @@
+package com.tmb.oneapp.productsexpservice.model.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class ApplicationStatusRequest {
+    private String citizenId;
+    private String mobileNo;
+
+}
