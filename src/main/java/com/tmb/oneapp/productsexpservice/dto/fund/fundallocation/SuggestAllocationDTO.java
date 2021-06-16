3fe0b8135f57305f96342d7a00f8f6cdd46722a5
Vendor376 <Vendor376@tcs.com>
2021-06-16 18:14:41 +0700

    remove unused import

--- src/main/java/com/tmb/oneapp/productsexpservice/dto/fund/fundallocation/SuggestAllocationDTO.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/dto/fund/fundallocation/SuggestAllocationDTO.java
@@ -1,6 +1,5 @@
 package com.tmb.oneapp.productsexpservice.dto.fund.fundallocation;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
