769ae00d589f2e9299cc3ea54d618da2eaa2b68b
kamonwan <kamonwan294@gmail.com>
2021-06-24 14:37:34 +0700

    get product credit list

--- src/main/java/com/tmb/oneapp/productsexpservice/controller/PersonalLoanController.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/controller/PersonalLoanController.java
@@ -96,7 +96,7 @@ public class PersonalLoanController {
     }
 
 
-    @GetMapping(value = "/get-product-list", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/get-product-loan-list", produces = MediaType.APPLICATION_JSON_VALUE)
     @LogAround
     @ApiOperation("Get product list")
     public ResponseEntity<TmbOneServiceResponse<List<ProductData>>> getProductList() {
@@ -114,6 +114,30 @@ public class PersonalLoanController {
             return ResponseEntity.ok().body(oneTmbOneServiceResponse);
         } catch (Exception e) {
             logger.error("error while get product list: {}", e);
+            oneTmbOneServiceResponse.setStatus(new TmbStatus(ResponseCode.FAILED.getCode(), ResponseCode.FAILED.getMessage(),
+                    ResponseCode.FAILED.getService()));
+            return ResponseEntity.badRequest().headers(responseHeaders).body(oneTmbOneServiceResponse);
+        }
+    }
+
+    @GetMapping(value = "/get-product-credit-list", produces = MediaType.APPLICATION_JSON_VALUE)
+    @LogAround
+    @ApiOperation("Get product list")
+    public ResponseEntity<TmbOneServiceResponse<List<ProductData>>> getProductCreditList() {
+        TmbOneServiceResponse<List<ProductData>> oneTmbOneServiceResponse = new TmbOneServiceResponse<>();
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set(ProductsExpServiceConstant.HEADER_TIMESTAMP, String.valueOf(Instant.now().toEpochMilli()));
+
+        try {
+            List<ProductData> productDataList = personalLoanService.getProductsCredit();
+            oneTmbOneServiceResponse.setData(productDataList);
+            oneTmbOneServiceResponse.setStatus(new TmbStatus(ProductsExpServiceConstant.SUCCESS_CODE,
+                    ProductsExpServiceConstant.SUCCESS_MESSAGE,
+                    ProductsExpServiceConstant.SERVICE_NAME, ProductsExpServiceConstant.SUCCESS_MESSAGE));
+            responseHeaders.set(ProductsExpServiceConstant.HEADER_TIMESTAMP, String.valueOf(Instant.now().toEpochMilli()));
+            return ResponseEntity.ok().body(oneTmbOneServiceResponse);
+        } catch (Exception e) {
+            logger.error("error while get product credit list: {}", e);
             oneTmbOneServiceResponse.setStatus(new TmbStatus(ResponseCode.FAILED.getCode(), ResponseCode.FAILED.getMessage(),
                     ResponseCode.FAILED.getService()));
             return ResponseEntity.badRequest().headers(responseHeaders).body(oneTmbOneServiceResponse);
