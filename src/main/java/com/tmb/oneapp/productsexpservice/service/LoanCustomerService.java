947e8d5d9a53ed190f56fd71f0110cd714f4ddb2
Nok <omerta@odds.team>
2021-06-25 14:36:34 +0700

    sort bank account

--- src/main/java/com/tmb/oneapp/productsexpservice/service/LoanCustomerService.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/service/LoanCustomerService.java
@@ -26,8 +26,8 @@ import org.springframework.stereotype.Service;
 import javax.xml.rpc.ServiceException;
 import java.math.BigDecimal;
 import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -56,6 +56,7 @@ public class LoanCustomerService {
     private static final BigDecimal RATE_VARIANCE_2 = BigDecimal.valueOf(23.00);
     private static final BigDecimal LIMIT_AMOUNT = BigDecimal.valueOf(500000);
     private static final BigDecimal AMOUNT_MIN = BigDecimal.valueOf(5000);
+    private int[] oders;
 
     public LoanCustomerResponse getCustomerProfile(String correlationId, LoanCustomerRequest request, String crmID) throws ServiceException, TMBCommonException, RemoteException {
         Facility facility = getFacility(request.getCaId());
@@ -190,16 +191,29 @@ public class LoanCustomerService {
         } catch (NullPointerException e) {
             logger.error("get account saving fail: ", e);
             throw e;
-        }catch (Exception ex) {
+        } catch (Exception ex) {
             logger.error("get account saving fail: ", ex);
             throw ex;
         }
 
-        for (DepositAccount acc : oneTmbOneServiceResponse.getData().getDepositAccountLists()) {
-            LoanCustomerDisburstAccount disburstAccount = new LoanCustomerDisburstAccount();
-            disburstAccount.setAccountNo(acc.getAccountNumber());
-            disburstAccount.setAccountName(acc.getProductNameTh());
-            disburstAccounts.add(disburstAccount);
+        List<DepositAccount> accList = oneTmbOneServiceResponse.getData().getDepositAccountLists();
+        int[] orders = new int[accList.size()];
+        for (int i = 0; i < accList.size(); i++) {
+            int order = Integer.parseInt(accList.get(i).getProductConfigSortOrder());
+            orders[i] = order;
+        }
+        Arrays.sort(orders);
+        for (int order1 : orders) {
+            for (DepositAccount acc : accList) {
+                int order2 = Integer.parseInt(acc.getProductConfigSortOrder());
+                var accNo = disburstAccounts.stream().filter(a -> a.getAccountNo() == acc.getAccountNumber()).findAny();
+                if (order1 == order2 && accNo.isEmpty()) {
+                    LoanCustomerDisburstAccount disburstAccount = new LoanCustomerDisburstAccount();
+                    disburstAccount.setAccountNo(acc.getAccountNumber());
+                    disburstAccount.setAccountName(acc.getProductNameTh());
+                    disburstAccounts.add(disburstAccount);
+                }
+            }
         }
 
         return disburstAccounts;
