4ee6e4694b0688d17a522fe9ccb3604d32e2ac41
Vendor376 <Vendor376@tcs.com>
2021-06-28 10:22:59 +0700

    add customer service request and enum for fatca

--- src/main/java/com/tmb/oneapp/productsexpservice/constant/ProductsExpServiceConstant.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/constant/ProductsExpServiceConstant.java
@@ -166,4 +166,6 @@ public class ProductsExpServiceConstant {
     public static final String CASE_NUMBER = "case_number";
     public static final String PTES_PORT_FOLIO_FLAG = "2";
     public static final String TRANSACTION_DATE = "transaction_date";
+
+    public static final String FATCA_FLAG = "fatca_flag";
 }
