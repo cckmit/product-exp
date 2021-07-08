2d5dae0d4a43c1860098610ef9ac766638d3ba93
kamonwan <kamonwan294@gmail.com>
2021-07-08 10:33:18 +0700

    change function name

--- src/main/java/com/tmb/oneapp/productsexpservice/controller/LoanSubmissionCustomerController.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/controller/LoanSubmissionCustomerController.java
@@ -8,7 +8,6 @@ import com.tmb.oneapp.productsexpservice.constant.ProductsExpServiceConstant;
 import com.tmb.oneapp.productsexpservice.constant.ResponseCode;
 import com.tmb.oneapp.productsexpservice.model.loan.LoanSubmissionResponse;
 import com.tmb.oneapp.productsexpservice.model.request.loan.LoanSubmissionRequest;
-import com.tmb.oneapp.productsexpservice.model.request.loan.LoanSubmitRegisterRequest;
 import com.tmb.oneapp.productsexpservice.service.LoanSubmissionCustomerService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -17,12 +16,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.time.Instant;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,17 +35,18 @@ public class LoanSubmissionCustomerController {
             new TMBLogger<>(LoanSubmissionCustomerController.class);
 
 
-    @GetMapping(value = "/get-customer-info", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/getIncomeInfo", produces = MediaType.APPLICATION_JSON_VALUE)
     @LogAround
     @ApiOperation("Get customer info")
-    public ResponseEntity<TmbOneServiceResponse<LoanSubmissionResponse>> getCustomerInfo(@Valid LoanSubmissionRequest request) {
-        TmbOneServiceResponse<LoanSubmissionResponse> oneTmbOneServiceResponse = new TmbOneServiceResponse<>();
+    public ResponseEntity<TmbOneServiceResponse<List<LoanSubmissionResponse>>> getIncomeInfo(@Valid @RequestHeader(ProductsExpServiceConstant.HEADER_CORRELATION_ID) String correlationId,
+                                                                                             @Valid LoanSubmissionRequest request) {
+        TmbOneServiceResponse<List<LoanSubmissionResponse>> oneTmbOneServiceResponse = new TmbOneServiceResponse<>();
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set(ProductsExpServiceConstant.HEADER_TIMESTAMP, String.valueOf(Instant.now().toEpochMilli()));
 
         try {
 
-            LoanSubmissionResponse loanSubmissionResponse = loanSubmissionCustomerService.getCustomerInfo(request.getCaID());
+            List<LoanSubmissionResponse> loanSubmissionResponse = loanSubmissionCustomerService.getCustomerInfo(request.getCaId());
             oneTmbOneServiceResponse.setData(loanSubmissionResponse);
             oneTmbOneServiceResponse.setStatus(new TmbStatus(ProductsExpServiceConstant.SUCCESS_CODE,
                     ProductsExpServiceConstant.SUCCESS_MESSAGE,
@@ -54,34 +55,7 @@ public class LoanSubmissionCustomerController {
             return ResponseEntity.ok().body(oneTmbOneServiceResponse);
 
         } catch (Exception e) {
-            logger.error("Error while getCustomerInfo: {}", e);
-            oneTmbOneServiceResponse.setStatus(new TmbStatus(ResponseCode.FAILED.getCode(), ResponseCode.FAILED.getMessage(),
-                    ResponseCode.FAILED.getService()));
-            return ResponseEntity.badRequest().headers(responseHeaders).body(oneTmbOneServiceResponse);
-        }
-
-    }
-
-    @PostMapping(value = "/submit-register", produces = MediaType.APPLICATION_JSON_VALUE)
-    @LogAround
-    @ApiOperation("Get customer info")
-    public ResponseEntity<TmbOneServiceResponse<LoanSubmitRegisterRequest>> submitRegister(@Valid LoanSubmitRegisterRequest loanSubmitRegisterRequest) {
-        TmbOneServiceResponse<LoanSubmitRegisterRequest> oneTmbOneServiceResponse = new TmbOneServiceResponse<>();
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set(ProductsExpServiceConstant.HEADER_TIMESTAMP, String.valueOf(Instant.now().toEpochMilli()));
-
-        try {
-
-            //LoanSubmitRegisterRequest response = loanSubmissionCustomerService.submitRegisterApplication();
-           // oneTmbOneServiceResponse.setData(response);
-            oneTmbOneServiceResponse.setStatus(new TmbStatus(ProductsExpServiceConstant.SUCCESS_CODE,
-                    ProductsExpServiceConstant.SUCCESS_MESSAGE,
-                    ProductsExpServiceConstant.SERVICE_NAME, ProductsExpServiceConstant.SUCCESS_MESSAGE));
-            responseHeaders.set(ProductsExpServiceConstant.HEADER_TIMESTAMP, String.valueOf(Instant.now().toEpochMilli()));
-            return ResponseEntity.ok().body(oneTmbOneServiceResponse);
-
-        }catch (Exception e) {
-            logger.error("Error while getCustomerInfo: {}", e);
+            logger.error("Error while getIncomeInfo: {}", e);
             oneTmbOneServiceResponse.setStatus(new TmbStatus(ResponseCode.FAILED.getCode(), ResponseCode.FAILED.getMessage(),
                     ResponseCode.FAILED.getService()));
             return ResponseEntity.badRequest().headers(responseHeaders).body(oneTmbOneServiceResponse);
