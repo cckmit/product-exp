a6ad4fe365a97664e5b867c5da6beb601ac31917
Vendor376 <Vendor376@tcs.com>
2021-07-08 18:11:44 +0700

    update enum error

--- src/main/java/com/tmb/oneapp/productsexpservice/enums/OpenPortfolioErrorEnums.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/enums/OpenPortfolioErrorEnums.java
@@ -8,8 +8,10 @@ public enum OpenPortfolioErrorEnums {
     AGE_NOT_OVER_TWENTY("2000025","Age is not over 20 years old","error"),
     NO_ACTIVE_CASA_ACCOUNT("2000019","No active CASA account","error"),
     FAILED_VERIFY_KYC("2000022","Failed Verify KYC Flag Or ID Card has expired","error"),
-    CUSTOMER_NOT_IN_LEVEL_FOUR("2000018","Customer is in risk level 4","error"),
+    CUSTOMER_IN_LEVEL_C3_AND_B3("2000018","Customer is in risk level C3 ,B3","error"),
+    CUSTOMER_IDENTIFY_ASSURANCE_LEVEL("2000018","If IAL level >= 210 and IAL <> Null then allow","error"),
     CUSTOMER_IN_RESTRICTED_LIST("2000018","Customer is in restricted list","error"),
+    CUSTOMER_HAS_US_NATIONALITY_OR_OTHER_THIRTY_RESTRICTED("2000018","Customer has U.S. nationality or other 30 restricted nationalities. Including non nationality.","error"),
     CUSTOMER_NOT_FILL_FATCA_FORM("2000034","Customer has not filled in the FATCA form","error"),
     ;
 
