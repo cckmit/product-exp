9ecfb9f79f69dd8cc535d3c7df9bf050f56ca269
Vendor376 <Vendor376@tcs.com>
2021-07-14 09:39:34 +0700

    fix sonar by make constant

--- src/main/java/com/tmb/oneapp/productsexpservice/enums/OpenPortfolioErrorEnums.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/enums/OpenPortfolioErrorEnums.java
@@ -4,17 +4,20 @@ import lombok.Getter;
 
 @Getter
 public enum OpenPortfolioErrorEnums {
-    NOT_IN_SERVICE_HOUR("2000001","Customer trade not in service available","error"),
-    AGE_NOT_OVER_TWENTY("2000025","Age is not over 20 years old","error"),
-    NO_ACTIVE_CASA_ACCOUNT("2000019","No active CASA account","error"),
-    FAILED_VERIFY_KYC("2000022","Failed Verify KYC Flag Or ID Card has expired","error"),
-    CUSTOMER_IN_LEVEL_C3_AND_B3("2000018","Customer is in risk level C3 ,B3","error"),
-    CUSTOMER_IDENTIFY_ASSURANCE_LEVEL("2000018","If IAL level >= 210 and IAL <> Null then allow","error"),
-    CUSTOMER_IN_RESTRICTED_LIST("2000018","Customer is in restricted list","error"),
-    CUSTOMER_HAS_US_NATIONALITY_OR_OTHER_THIRTY_RESTRICTED("2000018","Customer has U.S. nationality or other 30 restricted nationalities. Including non nationality.","error"),
-    CUSTOMER_NOT_FILL_FATCA_FORM("2000034","Customer has not filled in the FATCA form","error"),
+
+    NOT_IN_SERVICE_HOUR("2000001","Customer trade not in service available",OpenPortfolioErrorEnums.errorDesc),
+    AGE_NOT_OVER_TWENTY("2000025","Age is not over 20 years old",OpenPortfolioErrorEnums.errorDesc),
+    NO_ACTIVE_CASA_ACCOUNT("2000019","No active CASA account",OpenPortfolioErrorEnums.errorDesc),
+    FAILED_VERIFY_KYC("2000022","Failed Verify KYC Flag Or ID Card has expired",OpenPortfolioErrorEnums.errorDesc),
+    CUSTOMER_IN_LEVEL_C3_AND_B3(OpenPortfolioErrorEnums.errorCode200018,"Customer is in risk level C3 ,B3",OpenPortfolioErrorEnums.errorDesc),
+    CUSTOMER_IDENTIFY_ASSURANCE_LEVEL(OpenPortfolioErrorEnums.errorCode200018,"If IAL level >= 210 and IAL <> Null then allow",OpenPortfolioErrorEnums.errorDesc),
+    CUSTOMER_IN_RESTRICTED_LIST(OpenPortfolioErrorEnums.errorCode200018,"Customer is in restricted list",OpenPortfolioErrorEnums.errorDesc),
+    CUSTOMER_HAS_US_NATIONALITY_OR_OTHER_THIRTY_RESTRICTED(OpenPortfolioErrorEnums.errorCode200018,"Customer has U.S. nationality or other 30 restricted nationalities. Including non nationality.",OpenPortfolioErrorEnums.errorDesc),
+    CUSTOMER_NOT_FILL_FATCA_FORM("2000034","Customer has not filled in the FATCA form",OpenPortfolioErrorEnums.errorDesc),
     ;
 
+    private static final String errorDesc = "error";
+    private static final String errorCode200018 = "2000018";
     private String code;
     private String msg;
     private String desc;
