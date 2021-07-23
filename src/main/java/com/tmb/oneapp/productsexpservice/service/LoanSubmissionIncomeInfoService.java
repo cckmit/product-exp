111298034e85eea9efb4ba9a942d75284a7b40e6
Nok <omerta@odds.team>
2021-07-23 12:51:33 +0700

    working status

--- src/main/java/com/tmb/oneapp/productsexpservice/service/LoanSubmissionIncomeInfoService.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/service/LoanSubmissionIncomeInfoService.java
@@ -1,10 +1,17 @@
 package com.tmb.oneapp.productsexpservice.service;
 
 import com.tmb.common.logger.TMBLogger;
+import com.tmb.common.model.CustGeneralProfileResponse;
+import com.tmb.common.model.TmbOneServiceResponse;
+import com.tmb.common.model.legacy.rsl.ws.dropdown.response.ResponseDropdown;
 import com.tmb.common.model.legacy.rsl.ws.incomemodel.response.ResponseIncomeModel;
+import com.tmb.oneapp.productsexpservice.feignclients.CustomerServiceClient;
+import com.tmb.oneapp.productsexpservice.feignclients.loansubmission.LoanSubmissionGetDropdownListClient;
 import com.tmb.oneapp.productsexpservice.feignclients.loansubmission.LoanSubmissionGetIncomeModelInfoClient;
 import com.tmb.oneapp.productsexpservice.model.response.IncomeInfo;
 import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.xml.rpc.ServiceException;
@@ -16,21 +23,54 @@ import java.util.Objects;
 public class LoanSubmissionIncomeInfoService {
     private static final TMBLogger<LoanSubmissionIncomeInfoService> logger = new TMBLogger<>(LoanSubmissionIncomeInfoService.class);
     private final LoanSubmissionGetIncomeModelInfoClient incomeModelInfoClient;
+    private CustomerServiceClient customerServiceClient;
+    private final LoanSubmissionGetDropdownListClient getDropdownListClient;
 
 
     public IncomeInfo getIncomeInfoByRmId(String rmId) throws ServiceException, RemoteException {
         try {
 
-            ResponseIncomeModel responseIncomeModel = incomeModelInfoClient.getIncomeInfo(rmId);
+            ResponseIncomeModel responseIncomeModel = incomeModelInfoClient.getIncomeInfo(StringUtils.right(rmId, 14));
             if (Objects.isNull(responseIncomeModel) || Objects.isNull(responseIncomeModel.getBody())) {
                 return null;
             }
+
             IncomeInfo incomeInfo = new IncomeInfo();
             incomeInfo.setIncomeAmount(responseIncomeModel.getBody().getIncomeModelAmt());
+            incomeInfo.setStatusWorking(getWorkingStatus(rmId));
+
             return incomeInfo;
         } catch (Exception e) {
             logger.error("getIncomeInfoByRmId got exception:{}", e);
             throw e;
         }
+    }
+
+    private String getWorkingStatus(String rmId) throws ServiceException, RemoteException {
+        ResponseEntity<TmbOneServiceResponse<CustGeneralProfileResponse>> responseWorkingProfileInfo = customerServiceClient
+                .getCustomerProfile(rmId);
+        if (Objects.nonNull(responseWorkingProfileInfo)
+                && Objects.nonNull(responseWorkingProfileInfo.getBody())
+                && Objects.nonNull(responseWorkingProfileInfo.getBody().getData())) {
+            return mapWorkingStatus(responseWorkingProfileInfo.getBody().getData().getOccupationCode());
+        }
+        return null;
+    }
+
+    private String mapWorkingStatus(String occupation_code) throws ServiceException, RemoteException {
+        ResponseDropdown getDropdownListResp = getDropdownListClient.getDropdownList("RM_OCCUPATION");
+
+        var list = getDropdownListResp.getBody().getCommonCodeEntries();
+        for (var item : list) {
+            if (item.getEntryCode().equals(occupation_code)) {
+                if (item.getExtValue1().equals("01")) {
+                    return "salary";
+                }
+                if (item.getExtValue1().equals("02")) {
+                    return "self_employed";
+                }
+            }
+        }
+        return null;
     }
 }
--- src/test/java/com/tmb/oneapp/productsexpservice/service/LoanSubmissionIncomeInfoServiceTest.java
+++ src/test/java/com/tmb/oneapp/productsexpservice/service/LoanSubmissionIncomeInfoServiceTest.java
@@ -1,8 +1,13 @@
 package com.tmb.oneapp.productsexpservice.service;
 
+import com.tmb.common.model.CustGeneralProfileResponse;
+import com.tmb.common.model.TmbOneServiceResponse;
+import com.tmb.common.model.legacy.rsl.common.ob.dropdown.CommonCodeEntry;
+import com.tmb.common.model.legacy.rsl.ws.dropdown.response.ResponseDropdown;
 import com.tmb.common.model.legacy.rsl.ws.incomemodel.response.Body;
 import com.tmb.common.model.legacy.rsl.ws.incomemodel.response.ResponseIncomeModel;
-import com.tmb.oneapp.productsexpservice.feignclients.loansubmission.LoanSubmissionGetFacilityInfoClient;
+import com.tmb.oneapp.productsexpservice.feignclients.CustomerServiceClient;
+import com.tmb.oneapp.productsexpservice.feignclients.loansubmission.LoanSubmissionGetDropdownListClient;
 import com.tmb.oneapp.productsexpservice.feignclients.loansubmission.LoanSubmissionGetIncomeModelInfoClient;
 import com.tmb.oneapp.productsexpservice.model.response.IncomeInfo;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +16,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
 
 import javax.xml.rpc.ServiceException;
 import java.math.BigDecimal;
@@ -25,23 +31,85 @@ class LoanSubmissionIncomeInfoServiceTest {
 
     @Mock
     private LoanSubmissionGetIncomeModelInfoClient loanSubmissionGetIncomeModelInfoClient;
+    @Mock
+    private CustomerServiceClient customerServiceClient;
+    @Mock
+    private LoanSubmissionGetDropdownListClient getDropdownListClient;
 
     LoanSubmissionIncomeInfoService loanSubmissionIncomeInfoService;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
-        loanSubmissionIncomeInfoService = new LoanSubmissionIncomeInfoService(loanSubmissionGetIncomeModelInfoClient);
+        loanSubmissionIncomeInfoService = new LoanSubmissionIncomeInfoService(loanSubmissionGetIncomeModelInfoClient, customerServiceClient, getDropdownListClient);
     }
 
     @Test
-    public void testGetIncomeInfoByRmId() throws ServiceException, RemoteException {
+    public void testGetIncomeInfoByRmIdNotReturnStatus() throws ServiceException, RemoteException {
         ResponseIncomeModel clientRes = new ResponseIncomeModel();
         Body body = new Body();
         body.setIncomeModelAmt(BigDecimal.valueOf(100));
         clientRes.setBody(body);
         when(loanSubmissionGetIncomeModelInfoClient.getIncomeInfo(any())).thenReturn(clientRes);
         IncomeInfo result = loanSubmissionIncomeInfoService.getIncomeInfoByRmId("rmId");
-        assertEquals(BigDecimal.valueOf(100),result.getIncomeAmount());
+        assertEquals(BigDecimal.valueOf(100), result.getIncomeAmount());
+    }
+
+    @Test
+    public void testGetIncomeInfoByRmIdReturnWithStatusSalary() throws ServiceException, RemoteException {
+        ResponseIncomeModel clientRes = new ResponseIncomeModel();
+        Body body = new Body();
+        body.setIncomeModelAmt(BigDecimal.valueOf(100));
+        clientRes.setBody(body);
+        when(loanSubmissionGetIncomeModelInfoClient.getIncomeInfo(any())).thenReturn(clientRes);
+
+        TmbOneServiceResponse<CustGeneralProfileResponse> customerModuleResponse = new TmbOneServiceResponse<CustGeneralProfileResponse>();
+        CustGeneralProfileResponse profile = new CustGeneralProfileResponse();
+        profile.setOccupationCode("306");
+        customerModuleResponse.setData(profile);
+        when(customerServiceClient.getCustomerProfile(any())).thenReturn(ResponseEntity.ok(customerModuleResponse));
+
+        ResponseDropdown dropdown = new ResponseDropdown();
+        CommonCodeEntry[] entrycodes = new CommonCodeEntry[1];
+        entrycodes[0] = new CommonCodeEntry();
+        entrycodes[0].setEntryCode("306");
+        entrycodes[0].setExtValue1("01");
+        com.tmb.common.model.legacy.rsl.ws.dropdown.response.Body dropdownBody = new com.tmb.common.model.legacy.rsl.ws.dropdown.response.Body();
+        dropdownBody.setCommonCodeEntries(entrycodes);
+        dropdown.setBody(dropdownBody);
+        when(getDropdownListClient.getDropdownList(any())).thenReturn(dropdown);
+
+        IncomeInfo result = loanSubmissionIncomeInfoService.getIncomeInfoByRmId("rmId");
+        assertEquals(BigDecimal.valueOf(100), result.getIncomeAmount());
+        assertEquals("salary", result.getStatusWorking());
+    }
+
+    @Test
+    public void testGetIncomeInfoByRmIdReturnWithStatusSelfEmployed() throws ServiceException, RemoteException {
+        ResponseIncomeModel clientRes = new ResponseIncomeModel();
+        Body body = new Body();
+        body.setIncomeModelAmt(BigDecimal.valueOf(100));
+        clientRes.setBody(body);
+        when(loanSubmissionGetIncomeModelInfoClient.getIncomeInfo(any())).thenReturn(clientRes);
+
+        TmbOneServiceResponse<CustGeneralProfileResponse> customerModuleResponse = new TmbOneServiceResponse<CustGeneralProfileResponse>();
+        CustGeneralProfileResponse profile = new CustGeneralProfileResponse();
+        profile.setOccupationCode("306");
+        customerModuleResponse.setData(profile);
+        when(customerServiceClient.getCustomerProfile(any())).thenReturn(ResponseEntity.ok(customerModuleResponse));
+
+        ResponseDropdown dropdown = new ResponseDropdown();
+        CommonCodeEntry[] entrycodes = new CommonCodeEntry[1];
+        entrycodes[0] = new CommonCodeEntry();
+        entrycodes[0].setEntryCode("306");
+        entrycodes[0].setExtValue1("02");
+        com.tmb.common.model.legacy.rsl.ws.dropdown.response.Body dropdownBody = new com.tmb.common.model.legacy.rsl.ws.dropdown.response.Body();
+        dropdownBody.setCommonCodeEntries(entrycodes);
+        dropdown.setBody(dropdownBody);
+        when(getDropdownListClient.getDropdownList(any())).thenReturn(dropdown);
+
+        IncomeInfo result = loanSubmissionIncomeInfoService.getIncomeInfoByRmId("rmId");
+        assertEquals(BigDecimal.valueOf(100), result.getIncomeAmount());
+        assertEquals("self_employed", result.getStatusWorking());
     }
 }
