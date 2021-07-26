af1f8c49af65f345dd47a38a00f14d60ad6824b1
kamonwan <kamonwan294@gmail.com>
2021-07-26 10:45:35 +0700

    create api personal info

---
+++ src/main/java/com/tmb/oneapp/productsexpservice/model/personaldetail/Address.java
@@ -0,0 +1,23 @@
+package com.tmb.oneapp.productsexpservice.model.personaldetail;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class Address {
+    private String no;
+    private String buildingName;
+    private String streetName;
+    private String postalCode;
+    private String country;
+    private String tumbol;
+    private String road;
+    private String moo;
+    private String amphur;
+    private String province;
+    private String floor;
+}
