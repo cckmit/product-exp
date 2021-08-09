9a8c997d6df9dff86c12085c18c7322d014080c8
Vendor376 <Vendor376@tcs.com>
2021-08-09 15:00:53 +0700

    add request body and reques to customer client

---
+++ src/main/java/com/tmb/oneapp/productsexpservice/service/productexperience/customer/calculatecustomerrisk/request/AddressModel.java
@@ -0,0 +1,46 @@
+package com.tmb.oneapp.productsexpservice.service.productexperience.customer.calculatecustomerrisk.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressModel {
+
+    public String building;
+
+    @JsonProperty("company_name")
+    public String companyName;
+
+    public String country;
+
+    public String district;
+
+    public String moo;
+
+    public String no;
+
+    @JsonProperty("phone_extension")
+    public String phoneExtension;
+
+    @JsonProperty("phone_no")
+    public String phoneNo;
+
+    @JsonProperty("postal_code")
+    public String postalCode;
+
+    public String province;
+
+    public String road;
+
+    public String soi;
+
+    @JsonProperty("sub_district")
+    public String subDistrict;
+
+}
---
+++ src/main/java/com/tmb/oneapp/productsexpservice/service/productexperience/customer/calculatecustomerrisk/request/EkycRiskCalculateRequest.java
@@ -0,0 +1,53 @@
+package com.tmb.oneapp.productsexpservice.service.productexperience.customer.calculatecustomerrisk.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EkycRiskCalculateRequest {
+    @JsonProperty("business_code")
+    public String businessCode;
+
+    @JsonProperty("card_id")
+    public String cardId;
+
+    @JsonProperty("dob")
+    public String dob;
+
+    @JsonProperty("dob_country")
+    public String dobCountry;
+
+    @JsonProperty("first_name")
+    public String firstName;
+
+    @JsonProperty("first_name_eng")
+    public String firstNameEng;
+
+    @JsonProperty("income_source_country")
+    public String incomeSourceCountry;
+
+    @JsonProperty("last_name")
+    public String lastName;
+
+    @JsonProperty("last_name_eng")
+    public String lastNameEng;
+
+    @JsonProperty("occupation_code")
+    public String occupationCode;
+
+    @JsonProperty("office_address")
+    public AddressModel officeAddress;
+
+    @JsonProperty("primary_address")
+    public AddressModel primaryAddress;
+
+    @JsonProperty("registered_address")
+    public AddressModel registeredAddress;
+
+}
