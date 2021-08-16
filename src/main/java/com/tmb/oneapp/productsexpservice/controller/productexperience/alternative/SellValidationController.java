1d3868589fa0e594a8422aaa09b76c100785ddbd
Vendor376 <Vendor376@tcs.com>
2021-08-16 15:08:25 +0700

    create four controller separate the task

---
+++ src/main/java/com/tmb/oneapp/productsexpservice/controller/productexperience/alternative/SellValidationController.java
@@ -0,0 +1,14 @@
+package com.tmb.oneapp.productsexpservice.controller.productexperience.alternative;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "for sell fund validation")
+@RequestMapping("/funds")
+@RestController
+public class SellValidationController {
+
+
+
+}
