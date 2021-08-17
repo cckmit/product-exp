1f073455a977909f214d4b6f46b9878e9a38a7d1
Vendor376 <Vendor376@tcs.com>
2021-08-17 16:57:29 +0700

    implement alternative sell controller service and unit test

--- src/main/java/com/tmb/oneapp/productsexpservice/controller/productexperience/alternative/SellValidationController.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/controller/productexperience/alternative/SellValidationController.java
@@ -1,14 +1,61 @@
 package com.tmb.oneapp.productsexpservice.controller.productexperience.alternative;
 
+import com.tmb.common.logger.LogAround;
+import com.tmb.common.model.TmbOneServiceResponse;
+import com.tmb.oneapp.productsexpservice.constant.ProductsExpServiceConstant;
+import com.tmb.oneapp.productsexpservice.model.productexperience.alternative.request.AlternativeRequest;
+import com.tmb.oneapp.productsexpservice.service.productexperience.alternative.SellAlternativeService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import static com.tmb.oneapp.productsexpservice.util.TmbStatusUtil.notFoundStatus;
 
 @Api(tags = "for sell fund validation")
 @RequestMapping("/funds")
 @RestController
 public class SellValidationController {
 
+    private final SellAlternativeService sellAlternativeService;
 
+    @Autowired
+    public SellValidationController(SellAlternativeService sellAlternativeService) {
+        this.sellAlternativeService = sellAlternativeService;
+    }
+
+    /**
+     * Description:- method for handle alternative sell
+     *
+     * @param correlationId            the correlation id
+     * @param crmId                    the crm id
+     * @param alternativeRequest
+     * @return return valid status code
+     */
+    @ApiOperation(value = "Validation alternative case, then return fund sheet")
+    @LogAround
+    @PostMapping(value = "/alternative/sell")
+    public ResponseEntity<TmbOneServiceResponse<String>> validationBuy(
+            @Valid @RequestHeader(ProductsExpServiceConstant.HEADER_X_CORRELATION_ID) String correlationId,
+            @Valid @RequestHeader(ProductsExpServiceConstant.HEADER_X_CRM_ID) String crmId) {
+
+        TmbOneServiceResponse<String> oneServiceResponse = sellAlternativeService.validationSell(correlationId,crmId);
+        if (!StringUtils.isEmpty(oneServiceResponse.getStatus())) {
+            if(ProductsExpServiceConstant.SUCCESS_CODE.equals(oneServiceResponse.getStatus().getCode())){
+                return ResponseEntity.ok(oneServiceResponse);
+            }else{
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(oneServiceResponse);
+            }
+        } else {
+            oneServiceResponse.setStatus(notFoundStatus());
+            return new ResponseEntity(oneServiceResponse, HttpStatus.NOT_FOUND);
+        }
+
+    }
 
 }
---
+++ src/main/java/com/tmb/oneapp/productsexpservice/service/productexperience/alternative/SellAlternativeService.java
@@ -0,0 +1,74 @@
+package com.tmb.oneapp.productsexpservice.service.productexperience.alternative;
+
+import com.tmb.common.logger.TMBLogger;
+import com.tmb.common.model.TmbOneServiceResponse;
+import com.tmb.common.model.TmbStatus;
+import com.tmb.oneapp.productsexpservice.constant.ProductsExpServiceConstant;
+import com.tmb.oneapp.productsexpservice.enums.AlternativeBuySellSwitchDcaErrorEnums;
+import com.tmb.oneapp.productsexpservice.model.productexperience.customer.search.response.CustomerSearchResponse;
+import com.tmb.oneapp.productsexpservice.service.productexperience.customer.CustomerService;
+import com.tmb.oneapp.productsexpservice.util.TmbStatusUtil;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SellAlternativeService {
+
+    private static final TMBLogger<SellAlternativeService> logger = new TMBLogger<>(SellAlternativeService.class);
+
+    private final AlternativeService alternativeService;
+
+    private final CustomerService customerService;
+
+    public SellAlternativeService(AlternativeService alternativeService,
+                                  CustomerService customerService) {
+        this.alternativeService = alternativeService;
+        this.customerService = customerService;
+    }
+
+    public TmbOneServiceResponse<String> validationSell(String correlationId, String crmId) {
+
+        TmbOneServiceResponse<String> tmbOneServiceResponse = new TmbOneServiceResponse();
+        try {
+            TmbStatus status = TmbStatusUtil.successStatus();
+            tmbOneServiceResponse.setStatus(status);
+
+            CustomerSearchResponse customerInfo = customerService.getCustomerInfo(correlationId,crmId);
+
+            // validate service hour
+            tmbOneServiceResponse.setStatus(alternativeService.validateServiceHour(correlationId, status));
+            if (!tmbOneServiceResponse.getStatus().getCode().equals(ProductsExpServiceConstant.SUCCESS_CODE)) {
+                tmbOneServiceResponse.getStatus().setCode(AlternativeBuySellSwitchDcaErrorEnums.NOT_IN_SERVICE_HOUR.getCode());
+                return tmbOneServiceResponse;
+            }
+
+            // validate age should > 20
+            tmbOneServiceResponse.setStatus(alternativeService.validateDateNotOverTwentyYearOld(customerInfo.getBirthDate(), status));
+            if (!tmbOneServiceResponse.getStatus().getCode().equals(ProductsExpServiceConstant.SUCCESS_CODE)) {
+                tmbOneServiceResponse.getStatus().setCode(AlternativeBuySellSwitchDcaErrorEnums.AGE_NOT_OVER_TWENTY.getCode());
+                return tmbOneServiceResponse;
+            }
+
+            // validate customer risk level
+            tmbOneServiceResponse.setStatus(alternativeService.validateCustomerRiskLevel(correlationId,customerInfo, status));
+            if (!tmbOneServiceResponse.getStatus().getCode().equals(ProductsExpServiceConstant.SUCCESS_CODE)) {
+                tmbOneServiceResponse.getStatus().setCode(AlternativeBuySellSwitchDcaErrorEnums.CUSTOMER_IN_LEVEL_C3_AND_B3.getCode());
+                return tmbOneServiceResponse;
+            }
+
+            // validate suitability expired
+            tmbOneServiceResponse.setStatus(alternativeService.validateSuitabilityExpired(correlationId, crmId, status));
+            if (!tmbOneServiceResponse.getStatus().getCode().equals(ProductsExpServiceConstant.SUCCESS_CODE)) {
+                return tmbOneServiceResponse;
+            }
+
+            return tmbOneServiceResponse;
+
+        } catch (Exception ex) {
+            logger.error("error : {}", ex);
+            tmbOneServiceResponse.setStatus(null);
+            tmbOneServiceResponse.setData(null);
+            return tmbOneServiceResponse;
+        }
+    }
+
+}
