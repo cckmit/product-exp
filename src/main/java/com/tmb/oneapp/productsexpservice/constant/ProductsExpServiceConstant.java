a74a2a5afb9cc4c7889ff6571a08db9eaba38a63
Vendor376 <Vendor376@tcs.com>
2021-08-23 18:07:29 +0700

    change diff for sonar

--- src/main/java/com/tmb/oneapp/productsexpservice/constant/ProductsExpServiceConstant.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/constant/ProductsExpServiceConstant.java
@@ -20,6 +20,7 @@ public class ProductsExpServiceConstant {
 	/* RESPONSE MESSAGE FORMAT */
 	public static final String INVESTMENT_SERVICE_RESPONSE = "Response from investment service : {}";
 	public static final String CUSTOMER_EXP_SERVICE_RESPONSE = "Response from customer-exp service : {}";
+	public static final String EXCEPTION_RESPONSE = "Error Exception : {}";
 
 	public static final String HEADER_TIMESTAMP = "Timestamp";
 	public static final String CONTENT_TYPE = "content-type";
