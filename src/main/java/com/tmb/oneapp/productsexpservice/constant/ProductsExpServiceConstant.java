c8a0002ac80c249204795f612127190fab38ebca
KONGKIAT-TCS <kongkiat.p@tcs.com>
2021-08-24 14:11:37 +0700

    19642 [Bank] - update type to filter portfolio list

--- src/main/java/com/tmb/oneapp/productsexpservice/constant/ProductsExpServiceConstant.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/constant/ProductsExpServiceConstant.java
@@ -6,7 +6,7 @@ import java.util.List;
  * Constant class for Application
  */
 public class ProductsExpServiceConstant {
-	
+
     /**
      * created private constructor so that no body can create object of this call
      */
@@ -20,7 +20,7 @@ public class ProductsExpServiceConstant {
     /* RESPONSE MESSAGE FORMAT */
     public static final String INVESTMENT_SERVICE_RESPONSE = "Response from investment service : {}";
     public static final String CUSTOMER_EXP_SERVICE_RESPONSE = "Response from customer-exp service : {}";
-	public static final String EXCEPTION_RESPONSE = "Error Exception : {}";
+    public static final String EXCEPTION_RESPONSE = "Error Exception : {}";
 
     public static final String HEADER_TIMESTAMP = "Timestamp";
     public static final String CONTENT_TYPE = "content-type";
@@ -241,5 +241,6 @@ public class ProductsExpServiceConstant {
     public static final String INVESTMENT_JOINT_FLAG_JOINT = "Y";
 
     /* INVESTMENT PORTFOLIO TYPE */
+    public static final String INVESTMENT_PORTFOLIO_NORMAL_TYPE = "n";
     public static final String INVESTMENT_PORTFOLIO_JOINT_TYPE = "j";
 }
