620d874b1257baeb4ab6eaef157bffd2733d6b59
phongsathon.k <phongsathon.k@tcs.com>
2021-09-02 17:26:25 +0700

    [NEW] add service fetch loan staging bar

---
+++ src/main/java/com/tmb/oneapp/productsexpservice/model/lending/loan/LoanStagingbarRequest.java
@@ -0,0 +1,26 @@
+package com.tmb.oneapp.productsexpservice.model.lending.loan;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@ToString
+public class LoanStagingbarRequest {
+	
+	private String loanType;
+	private String productHeaderKey;
+}
