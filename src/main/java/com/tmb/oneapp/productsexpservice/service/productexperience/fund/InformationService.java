a4604c0c13dac7dd1d59fb13777ac25beefe02a4
Vendor376 <Vendor376@tcs.com>
2021-09-27 16:45:49 +0700

    fix test fundinformation error

--- src/main/java/com/tmb/oneapp/productsexpservice/service/productexperience/fund/InformationService.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/service/productexperience/fund/InformationService.java
@@ -1,5 +1,6 @@
 package com.tmb.oneapp.productsexpservice.service.productexperience.fund;
 
+import com.tmb.common.exception.model.TMBCommonException;
 import com.tmb.common.logger.LogAround;
 import com.tmb.common.logger.TMBLogger;
 import com.tmb.oneapp.productsexpservice.constant.ProductsExpServiceConstant;
@@ -7,6 +8,7 @@ import com.tmb.oneapp.productsexpservice.dto.fund.information.InformationDto;
 import com.tmb.oneapp.productsexpservice.model.productexperience.fund.dailynav.response.DailyNavBody;
 import com.tmb.oneapp.productsexpservice.model.productexperience.fund.information.request.FundCodeRequestBody;
 import com.tmb.oneapp.productsexpservice.model.productexperience.fund.information.response.InformationBody;
+import com.tmb.oneapp.productsexpservice.service.productexperience.TmbErrorHandle;
 import com.tmb.oneapp.productsexpservice.service.productexperience.async.InvestmentAsyncService;
 import com.tmb.oneapp.productsexpservice.util.UtilMap;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +16,13 @@ import org.springframework.stereotype.Service;
 
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 /**
  * InformationService class will get data from api services, and handle business criteria
  */
 @Service
-public class InformationService {
+public class InformationService extends TmbErrorHandle {
 
     private static final TMBLogger<InformationService> logger = new TMBLogger<>(InformationService.class);
 
@@ -38,7 +41,7 @@ public class InformationService {
      * @return InformationDto
      */
     @LogAround
-    public InformationDto getFundInformation(String correlationId, FundCodeRequestBody fundCodeRequestBody) {
+    public InformationDto getFundInformation(String correlationId, FundCodeRequestBody fundCodeRequestBody) throws TMBCommonException {
         Map<String, String> investmentRequestHeader = UtilMap.createHeader(correlationId);
         try {
             CompletableFuture<InformationBody> fetchFundInformation = investmentAsyncService.fetchFundInformation(investmentRequestHeader, fundCodeRequestBody);
@@ -48,9 +51,14 @@ public class InformationService {
                     .information(fetchFundInformation.get())
                     .dailyNav(fetchFundDailyNav.get())
                     .build();
+        } catch (ExecutionException e) {
+            if(e.getCause() instanceof TMBCommonException){
+                throw (TMBCommonException) e.getCause();
+            }
+            failedErrorHandle();
         } catch (Exception ex) {
             logger.error(ProductsExpServiceConstant.EXCEPTION_OCCURRED, ex);
-            return null;
         }
+        return null;
     }
 }
