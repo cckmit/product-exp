ab2870ae45c2b28ceb217a898b45010c8ca1065e
Vendor376 <Vendor376@tcs.com>
2021-10-05 12:45:00 +0700

    add fundhouse model

---
+++ src/main/java/com/tmb/oneapp/productsexpservice/model/common/findbyfundhouse/FundHouseBankData.java
@@ -0,0 +1,42 @@
+package com.tmb.oneapp.productsexpservice.model.common.findbyfundhouse;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Setter
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@ToString
+public class FundHouseBankData {
+
+    @JsonIgnoreProperties(value = "fund_house_code")
+    private String fundHouseCode;
+
+    @JsonProperty(value = "fund_house_name_en")
+    private String fundHouseNameEn;
+
+    @JsonProperty(value="fund_house_name_th")
+    private String fundHouseNameTh;
+
+    @JsonProperty(value = "to_account_no")
+    private String toAccountNo;
+
+    @JsonProperty(value = "account_type")
+    private String accountType;
+
+    @JsonProperty(value = "financial_id")
+    private String financialId;
+
+    @JsonProperty(value = "ltf_merchant_id")
+    private String ltfMerchantId;
+
+    @JsonProperty(value = "rmf_merchant_id")
+    private String rmfMerchantId;
+
+}
+
+
