ab38c71c6158785034096786937313cd79d0558f
Oranuch Charoennan <oranuch@odds.team>
2021-10-12 22:58:32 +0700

    refactor response header of lending apis.

--- src/main/java/com/tmb/oneapp/productsexpservice/controller/LoanCustomerController.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/controller/LoanCustomerController.java
@@ -3,24 +3,22 @@ package com.tmb.oneapp.productsexpservice.controller;
 import com.tmb.common.logger.LogAround;
 import com.tmb.common.logger.TMBLogger;
 import com.tmb.common.model.TmbOneServiceResponse;
-import com.tmb.common.model.TmbStatus;
+import com.tmb.common.util.TMBUtils;
 import com.tmb.oneapp.productsexpservice.constant.ProductsExpServiceConstant;
-import com.tmb.oneapp.productsexpservice.constant.ResponseCode;
 import com.tmb.oneapp.productsexpservice.model.request.loan.LoanCustomerRequest;
 import com.tmb.oneapp.productsexpservice.model.request.loan.LoanCustomerSubmissionRequest;
 import com.tmb.oneapp.productsexpservice.model.response.loan.LoanCustomerResponse;
 import com.tmb.oneapp.productsexpservice.model.response.loan.LoanCustomerSubmissionResponse;
 import com.tmb.oneapp.productsexpservice.service.LoanCustomerService;
+import com.tmb.oneapp.productsexpservice.util.TmbStatusUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.time.Instant;
 
 @RestController
 @RequiredArgsConstructor
@@ -38,24 +36,18 @@ public class LoanCustomerController {
                                                                                               @Valid @RequestHeader(name = ProductsExpServiceConstant.HEADER_X_CORRELATION_ID) String correlationId,
                                                                                               @Valid LoanCustomerRequest request) {
 
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set(ProductsExpServiceConstant.HEADER_TIMESTAMP, String.valueOf(Instant.now().toEpochMilli()));
         TmbOneServiceResponse<LoanCustomerResponse> oneTmbOneServiceResponse = new TmbOneServiceResponse<>();
 
         try {
-            LoanCustomerResponse loanCustomerResponse = loanCustomerService.getCustomerProfile(correlationId, request,crmId);
+            LoanCustomerResponse loanCustomerResponse = loanCustomerService.getCustomerProfile(correlationId, request, crmId);
             oneTmbOneServiceResponse.setData(loanCustomerResponse);
-            oneTmbOneServiceResponse.setStatus(new TmbStatus(ProductsExpServiceConstant.SUCCESS_CODE,
-                    ProductsExpServiceConstant.SUCCESS_MESSAGE,
-                    ProductsExpServiceConstant.SERVICE_NAME, ProductsExpServiceConstant.SUCCESS_MESSAGE));
+            oneTmbOneServiceResponse.setStatus(TmbStatusUtil.successStatus());
+            return ResponseEntity.ok().headers(TMBUtils.getResponseHeaders()).body(oneTmbOneServiceResponse);
 
-            responseHeaders.set("Timestamp", String.valueOf(Instant.now().toEpochMilli()));
-            return ResponseEntity.ok().body(oneTmbOneServiceResponse);
         } catch (Exception e) {
             logger.error("Error while getConfig: {}", e);
-            oneTmbOneServiceResponse.setStatus(new TmbStatus(ResponseCode.FAILED.getCode(), ResponseCode.FAILED.getMessage(),
-                    ResponseCode.FAILED.getService()));
-            return ResponseEntity.badRequest().headers(responseHeaders).body(oneTmbOneServiceResponse);
+            oneTmbOneServiceResponse.setStatus(TmbStatusUtil.failedStatus());
+            return ResponseEntity.badRequest().headers(TMBUtils.getResponseHeaders()).body(oneTmbOneServiceResponse);
         }
 
     }
@@ -65,25 +57,18 @@ public class LoanCustomerController {
     @PostMapping(value = "/submission-customer-profile", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<TmbOneServiceResponse<LoanCustomerSubmissionResponse>> saveCustomerProfile(@Valid @RequestHeader(ProductsExpServiceConstant.HEADER_X_CORRELATION_ID) String correlationId,
                                                                                                      @Valid @RequestBody LoanCustomerSubmissionRequest request) {
-
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set(ProductsExpServiceConstant.HEADER_TIMESTAMP, String.valueOf(Instant.now().toEpochMilli()));
         TmbOneServiceResponse<LoanCustomerSubmissionResponse> oneTmbOneServiceResponse = new TmbOneServiceResponse<>();
 
         try {
             LoanCustomerSubmissionResponse loanCustomerSubmissionResponse = loanCustomerService.saveCustomerSubmission(request);
             oneTmbOneServiceResponse.setData(loanCustomerSubmissionResponse);
-            oneTmbOneServiceResponse.setStatus(new TmbStatus(ProductsExpServiceConstant.SUCCESS_CODE,
-                    ProductsExpServiceConstant.SUCCESS_MESSAGE,
-                    ProductsExpServiceConstant.SERVICE_NAME, ProductsExpServiceConstant.SUCCESS_MESSAGE));
+            oneTmbOneServiceResponse.setStatus(TmbStatusUtil.successStatus());
+            return ResponseEntity.ok().headers(TMBUtils.getResponseHeaders()).body(oneTmbOneServiceResponse);
 
-            responseHeaders.set("Timestamp", String.valueOf(Instant.now().toEpochMilli()));
-            return ResponseEntity.ok().body(oneTmbOneServiceResponse);
         } catch (Exception e) {
             logger.error("Error while submission customer profile : {}", e);
-            oneTmbOneServiceResponse.setStatus(new TmbStatus(ResponseCode.FAILED.getCode(), ResponseCode.FAILED.getMessage(),
-                    ResponseCode.FAILED.getService()));
-            return ResponseEntity.badRequest().headers(responseHeaders).body(oneTmbOneServiceResponse);
+            oneTmbOneServiceResponse.setStatus(TmbStatusUtil.failedStatus());
+            return ResponseEntity.badRequest().headers(TMBUtils.getResponseHeaders()).body(oneTmbOneServiceResponse);
         }
 
     }
--- src/main/java/com/tmb/oneapp/productsexpservice/controller/PersonalLoanController.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/controller/PersonalLoanController.java
@@ -3,9 +3,8 @@ package com.tmb.oneapp.productsexpservice.controller;
 import com.tmb.common.logger.LogAround;
 import com.tmb.common.logger.TMBLogger;
 import com.tmb.common.model.TmbOneServiceResponse;
-import com.tmb.common.model.TmbStatus;
+import com.tmb.common.util.TMBUtils;
 import com.tmb.oneapp.productsexpservice.constant.ProductsExpServiceConstant;
-import com.tmb.oneapp.productsexpservice.constant.ResponseCode;
 import com.tmb.oneapp.productsexpservice.model.flexiloan.InstantLoanCalUWResponse;
 import com.tmb.oneapp.productsexpservice.model.request.loan.InstantLoanCalUWRequest;
 import com.tmb.oneapp.productsexpservice.model.request.loan.LoanPreloadRequest;
@@ -14,12 +13,12 @@ import com.tmb.oneapp.productsexpservice.model.response.loan.ApplyPersonalLoan;
 import com.tmb.oneapp.productsexpservice.model.response.loan.ProductData;
 import com.tmb.oneapp.productsexpservice.service.LoanSubmissionOnlineService;
 import com.tmb.oneapp.productsexpservice.service.PersonalLoanService;
+import com.tmb.oneapp.productsexpservice.util.TmbStatusUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,7 +27,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.time.Instant;
 import java.util.List;
 
 @RestController
@@ -40,7 +38,6 @@ public class PersonalLoanController {
     private final PersonalLoanService personalLoanService;
     private final LoanSubmissionOnlineService loanSubmissionOnlineService;
     private static final TMBLogger<ProductsVerifyCvvController> logger = new TMBLogger<>(ProductsVerifyCvvController.class);
-    private static final HttpHeaders responseHeaders = new HttpHeaders();
 
     @GetMapping("/get_preload")
     @LogAround
@@ -54,13 +51,12 @@ public class PersonalLoanController {
 
         try {
             oneTmbOneServiceResponse.setData(personalLoanService.checkPreload(correlationId, loanPreloadRequest));
-            oneTmbOneServiceResponse.setStatus(getStatusSuccess());
-            setHeader();
-            return ResponseEntity.ok().body(oneTmbOneServiceResponse);
+            oneTmbOneServiceResponse.setStatus(TmbStatusUtil.successStatus());
+            return ResponseEntity.ok().headers(TMBUtils.getResponseHeaders()).body(oneTmbOneServiceResponse);
         } catch (Exception e) {
             logger.error("error while getConfig: {}", e);
-            oneTmbOneServiceResponse.setStatus(getStatusFailed());
-            return ResponseEntity.badRequest().headers(responseHeaders).body(oneTmbOneServiceResponse);
+            oneTmbOneServiceResponse.setStatus(TmbStatusUtil.failedStatus());
+            return ResponseEntity.badRequest().headers(TMBUtils.getResponseHeaders()).body(oneTmbOneServiceResponse);
         }
     }
 
@@ -72,13 +68,12 @@ public class PersonalLoanController {
         try {
             InstantLoanCalUWResponse instantLoanCalUWResponse = loanSubmissionOnlineService.checkCalculateUnderwriting(instantLoanCalUWRequest);
             oneTmbOneServiceResponse.setData(instantLoanCalUWResponse);
-            oneTmbOneServiceResponse.setStatus(getStatusSuccess());
-            setHeader();
-            return ResponseEntity.ok().body(oneTmbOneServiceResponse);
+            oneTmbOneServiceResponse.setStatus(TmbStatusUtil.successStatus());
+            return ResponseEntity.ok().headers(TMBUtils.getResponseHeaders()).body(oneTmbOneServiceResponse);
         } catch (Exception e) {
             logger.error("error while check under writing: {}", e);
-            oneTmbOneServiceResponse.setStatus(getStatusFailed());
-            return ResponseEntity.badRequest().headers(responseHeaders).body(oneTmbOneServiceResponse);
+            oneTmbOneServiceResponse.setStatus(TmbStatusUtil.failedStatus());
+            return ResponseEntity.badRequest().headers(TMBUtils.getResponseHeaders()).body(oneTmbOneServiceResponse);
         }
     }
 
@@ -92,13 +87,12 @@ public class PersonalLoanController {
         try {
             ApplyPersonalLoan productDataLoanList = personalLoanService.getProductsLoan();
             oneTmbOneServiceResponse.setData(productDataLoanList);
-            oneTmbOneServiceResponse.setStatus(getStatusSuccess());
-            setHeader();
-            return ResponseEntity.ok().body(oneTmbOneServiceResponse);
+            oneTmbOneServiceResponse.setStatus(TmbStatusUtil.successStatus());
+            return ResponseEntity.ok().headers(TMBUtils.getResponseHeaders()).body(oneTmbOneServiceResponse);
         } catch (Exception e) {
             logger.error("error while get product list: {}", e);
-            oneTmbOneServiceResponse.setStatus(getStatusFailed());
-            return ResponseEntity.badRequest().headers(responseHeaders).body(oneTmbOneServiceResponse);
+            oneTmbOneServiceResponse.setStatus(TmbStatusUtil.failedStatus());
+            return ResponseEntity.badRequest().headers(TMBUtils.getResponseHeaders()).body(oneTmbOneServiceResponse);
         }
     }
 
@@ -111,31 +105,13 @@ public class PersonalLoanController {
         try {
             List<ProductData> productDataCreditList = personalLoanService.getProductsCredit();
             oneTmbOneServiceResponse.setData(productDataCreditList);
-            oneTmbOneServiceResponse.setStatus(getStatusSuccess());
-            setHeader();
+            oneTmbOneServiceResponse.setStatus(TmbStatusUtil.successStatus());
             return ResponseEntity.ok().body(oneTmbOneServiceResponse);
         } catch (Exception e) {
             logger.error("error while get product credit list: {}", e);
-            oneTmbOneServiceResponse.setStatus(getStatusFailed());
-            return ResponseEntity.badRequest().headers(responseHeaders).body(oneTmbOneServiceResponse);
+            oneTmbOneServiceResponse.setStatus(TmbStatusUtil.failedStatus());
+            return ResponseEntity.badRequest().headers(TMBUtils.getResponseHeaders()).body(oneTmbOneServiceResponse);
         }
     }
-
-    private TmbStatus getStatusFailed() {
-        return new TmbStatus(ResponseCode.FAILED.getCode(), ResponseCode.FAILED.getMessage(),
-                ResponseCode.FAILED.getService());
-    }
-
-
-    private TmbStatus getStatusSuccess() {
-        return new TmbStatus(ProductsExpServiceConstant.SUCCESS_CODE,
-                ProductsExpServiceConstant.SUCCESS_MESSAGE,
-                ProductsExpServiceConstant.SERVICE_NAME, ProductsExpServiceConstant.SUCCESS_MESSAGE);
-    }
-
-    private void setHeader() {
-        responseHeaders.set(ProductsExpServiceConstant.HEADER_TIMESTAMP, String.valueOf(Instant.now().toEpochMilli()));
-    }
-
 
 }
