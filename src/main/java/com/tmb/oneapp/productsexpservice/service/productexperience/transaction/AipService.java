84ef8aafd325b9344fd7a1ea38764cfdeffaa2dd
Vendor376 <Vendor376@tcs.com>
2021-11-16 14:48:58 +0700

    implement aip service

---
+++ src/main/java/com/tmb/oneapp/productsexpservice/service/productexperience/transaction/AipService.java
@@ -0,0 +1,71 @@
+package com.tmb.oneapp.productsexpservice.service.productexperience.transaction;
+
+import com.tmb.common.exception.model.TMBCommonException;
+import com.tmb.common.logger.LogAround;
+import com.tmb.common.logger.TMBLogger;
+import com.tmb.common.model.TmbOneServiceResponse;
+import com.tmb.oneapp.productsexpservice.constant.ProductsExpServiceConstant;
+import com.tmb.oneapp.productsexpservice.feignclients.CreditCardClient;
+import com.tmb.oneapp.productsexpservice.feignclients.InvestmentRequestClient;
+import com.tmb.oneapp.productsexpservice.model.activatecreditcard.CreditCardDetail;
+import com.tmb.oneapp.productsexpservice.model.activatecreditcard.FetchCardResponse;
+import com.tmb.oneapp.productsexpservice.model.productexperience.transaction.orderaip.request.OrderAIPRequestBody;
+import com.tmb.oneapp.productsexpservice.model.productexperience.transaction.orderaip.response.OrderAIPResponseBody;
+import com.tmb.oneapp.productsexpservice.util.UtilMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
+
+@Service
+public class AipService {
+
+    private static final TMBLogger<AipService> logger = new TMBLogger<>(AipService.class);
+
+    private final InvestmentRequestClient investmentRequestClient;
+
+    private final CreditCardClient creditCardClient;
+
+    @Autowired
+    public AipService(InvestmentRequestClient investmentRequestClient, CreditCardClient creditCardClient) {
+        this.investmentRequestClient = investmentRequestClient;
+        this.creditCardClient = creditCardClient;
+    }
+
+    /**
+     * Method createAipOrder
+     *
+     * @param correlationId
+     * @param crmId
+     * @param orderAIPRequestBody
+     */
+    @LogAround
+    public TmbOneServiceResponse<OrderAIPResponseBody> createAipOrder(String correlationId, String crmId, OrderAIPRequestBody orderAIPRequestBody) {
+        TmbOneServiceResponse<OrderAIPResponseBody> tmbOneServiceResponse = new TmbOneServiceResponse();
+        try {
+
+            // credit card replace expiry date
+            if("C".equals(orderAIPRequestBody.getOrderType())){
+                ResponseEntity<FetchCardResponse> cardResponse = creditCardClient.getCreditCardDetails(correlationId, orderAIPRequestBody.getBankAccountId());
+                CreditCardDetail creditCard = cardResponse.getBody().getCreditCard();
+                String creditCardExpiry = creditCard.getCardInfo().getExpiredBy();
+                if(StringUtils.isEmpty(creditCardExpiry))
+                    throw new TMBCommonException("creditCardExpiry is empty");
+                orderAIPRequestBody.setCreditCardExpiry(creditCardExpiry);
+            }
+
+            Map<String, String> investmentRequestHeader = UtilMap.createHeaderWithCrmId(correlationId, crmId);
+            ResponseEntity<TmbOneServiceResponse<OrderAIPResponseBody>> oneServiceResponseResponseEntity = investmentRequestClient.createAipOrder(investmentRequestHeader,orderAIPRequestBody);
+            return oneServiceResponseResponseEntity.getBody();
+
+        } catch (Exception ex) {
+            logger.error(ProductsExpServiceConstant.EXCEPTION_OCCURRED, ex);
+            tmbOneServiceResponse.setStatus(null);
+            tmbOneServiceResponse.setData(null);
+            return tmbOneServiceResponse;
+        }
+    }
+
+}
