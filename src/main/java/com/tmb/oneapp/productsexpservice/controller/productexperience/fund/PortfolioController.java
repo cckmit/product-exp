7016910aabec03538d221fadbf4bca2f3fdc7900
KONGKIAT-TCS <kongkiat.p@tcs.com>
2021-12-19 15:01:08 +0700

    TECH [Bank] - fix sonar issue by optimize imports

--- src/main/java/com/tmb/oneapp/productsexpservice/controller/productexperience/fund/PortfolioController.java
+++ src/main/java/com/tmb/oneapp/productsexpservice/controller/productexperience/fund/PortfolioController.java
@@ -13,14 +13,12 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.time.Instant;
 
 /**
  * PortfolioController will handle to call apis for getting portfolio list
